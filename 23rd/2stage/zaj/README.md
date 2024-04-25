758efc14513707ae846213d69b13c6f9b9d8830a
TheLordOfCat <franciszekjarz@gmail.com>
2024-04-25 15:41:59 +0200

    school progress

---
+++ 23rd/2stage/zaj/README.md
@@ -0,0 +1,6 @@
+# n Olimpiada Informatyczna
+23rd Olimpiad: https://oi.edu.pl/l/288/ <br />
+Szkopuł:  https://szkopul.edu.pl/problemset/problem/Orc2Z7ti1xLaUUQDT1a6RGR5/site/?key=statement <br />
+Solution: https://oi.edu.pl/static/attachment/20171006/oi23.pdf#page=117 <br />
+Time Complexity: <br />
+Points:  <br />
---
+++ 23rd/2stage/zaj/tester.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define PB push_back
+
+int n, m;
+vector<int> A, B;
+
+void getData() {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    A.PB(temp);
+  }
+  for (int i = 0; i < m; i++) {
+    int temp;
+    cin >> temp;
+    B.PB(temp);
+  }
+}
+
+void getRandom() {
+  srand(time(0));
+  n = rand() % 10 + 1;
+  m = rand() % 15 + 1;
+  for (int i = 0; i < n; i++) {
+    int temp = rand() % 10 + 1;
+    A.PB(temp);
+  }
+  for (int i = 0; i < m; i++) {
+    int temp = rand() % 10 + 1;
+    B.PB(temp);
+  }
+}
+
+void printData() {
+  cout << n << " " << m << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << A[i] << " ";
+  }
+  cout << "\n";
+  for (int i = 0; i < m; i++) {
+    cout << B[i] << " ";
+  }
+  cout << "\n";
+}
+
+int brute() {}
+
+int solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int op = 1;
+  for (int test = 1; test <= 1; test++) {
+    cout << "TEST nr." << test << " = ";
+    if (op == 1) {
+      getData();
+    } else {
+      getRandom();
+    }
+    int ansB = brute();
+    int ansS = solve();
+    if (ansB != ansS) {
+      cout << "ERROR\n";
+      cout << "BRUTE: " << ansB << "\n";
+      cout << "SOLVE: " << ansS << "\n";
+      return 0;
+    }
+    cout << "CORRECT\n";
+  }
+  return 0;
+}
