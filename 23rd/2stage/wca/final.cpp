5155febe52278938d76b70a2a484dd8adfcde2ad
TheLordOfCat <franciszekjarz@gmail.com>
2024-05-01 09:56:42 +0200

    WCA coding

--- 23rd/2stage/wca/final.cpp
+++ 23rd/2stage/wca/final.cpp
@@ -1,10 +1,56 @@
 #include<iostream>
+#include<vector>
+#include <queue>
+#include <cmath>
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
 
 using namespace std;
 
+using ll = long long int;
+
+int n;
+vector<int> a;
+
+void getData(){
+    cin>>n;
+    for(int i =0 ; i<n; i++){
+        int temp;
+        cin>>temp;
+        a.PB(temp);
+    }
+}
+
+struct pairCompare {
+    bool operator()(PII a, PII b) {
+        return a.first+a.second < b.first+b.second;
+    }
+};
+
+ll floorLog2(ll n) {
+    return floor(log2(n)) + 1;
+}
+
+ll solve(){
+    ll ans = 0;
+
+    for(int i = 0; i<n; i++){
+       ans += floorLog2(a[i])*2; 
+    }
+
+    return ans;
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    getData();
+    ll ansS = solve();
+    cout<<ansS<<"\n";
 
     return 0;
 }
