809f4254890a1793f3a15f162be15c441ed953a8
TheLordOfCat <franciszekjarz@gmail.com>
2024-05-02 10:00:43 +0200

    LOG coded

--- 22nd/2stage/log/README.md
+++ 22nd/2stage/log/README.md
@@ -1,4 +1,4 @@
-# 22nd Olimpiada Informatyczna
+#22nd Olimpiada Informatyczna
 22nd Olimpiad:  https://oi.edu.pl/l/274/<br />
 Szkopuł:  https://szkopul.edu.pl/problemset/problem/ShrCwRqlzMKLBb269GkVbWg_/site/?key=statement<br />
 Solution:  http://oi.edu.pl/static/attachment/20160825/oi22.pdf#page=91<br />
--- 22nd/2stage/log/tester.cpp
+++ 22nd/2stage/log/tester.cpp
@@ -7,36 +7,64 @@ using namespace std;
 #define PB push_back
 #define PII pair<int,int>
 
+using ll = long long int;
+
+const int MAXN = 1'000'000;
+const int MAXM = 1'000'000;
+
+pair<int, ll> operator + (const pair<int, ll>& a, const pair<int, ll> b)
+{
+	return MP(a.first + b.first, a.second + b.second);
+}
+
 int n, m;
-vector<pair<char,PII>> request;
+vector<pair<char,pair<int,ll>>> request;
 
 void getData(){
     cin>>n>>m;
     for(int i =0; i <m; i++){
         char c;
-        int a,b;
+        int a;
+        ll b;
         cin>>c;
         cin>>a>>b;
         request.PB(MP(c,MP(a,b)));
     }
 } 
 
+pair<int,ll> tree[4*MAXN+1];
+int val[MAXN+1];
+int license[MAXN+1];
+int R = 1;
+int depth = 1;
+
 void getRandom(){
     srand(time(0));
     n = rand()%10+1;
     m = rand()%10+1;
+    request.clear();
     for(int i = 0; i<m; i++){
         int type = rand()%2+1;
         if(type == 1){ //U
             int k = rand()%n+1;
-            int a = rand()%10;
+            ll a = rand()%10;
             request.PB(MP('Z',MP(k,a)));
         }else if(type == 2){ //Z
             int c = rand()%n+1;
-            int s = rand()%10+1;
+            ll s = rand()%10+1;
             request.PB(MP('Z',MP(c,s)));
         }
     }
+        
+    for(int i = 0; i<4*MAXN+1; i++){
+        tree[i] = MP(0,0);
+    }
+    for(int i = 0; i<MAXN+1; i++){
+        val[i] = 0;
+        license[i] = 0;
+    }
+    R = 1;
+    depth = 1;
 }
 
 void printData(){
@@ -47,12 +75,135 @@ void printData(){
 }
 
 vector<bool> brute(){
+    vector<bool> ans;
+    vector<int> drivers(n+1, 0);
+    for(int i = 0; i<m; i++){
+        if(request[i].first == 'U'){
+            PII cur = request[i].second;
 
+            drivers[cur.first] = cur.second;    
+        }else if(request[i].first == 'Z'){
+            PII cur = request[i].second;
+
+            ll sum = 0;
+            for(int j = 1;j <= n;j ++){
+                sum += min(cur.second, drivers[j]);
+            }
+            if(sum >= (ll)cur.first*(ll)cur.second){
+                ans.PB(true);
+            }else{
+                ans.PB(false);
+            }
+        }
+    }
+    return ans;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+int left(int v){
+    return 2*v;
+}
+
+int right(int v){
+    return 2*v+1;
+}
+
+int leaf(int v){
+    return R+v;
+}
+
+void update(int v, pair<int,ll> change){
+    int V = leaf(v);
+    tree[V] = tree[V] + change;
+    while(V > 1){
+        V /= 2;
+        tree[V] = tree[left(V)] + tree[right(V)];
+    }
+}
+
+int findPos(ll value){
+    int b = 0, e = m;
+    while(b < e){
+        int mid = (b+e)/2;
+        if(val[mid] <= value){
+            e = mid;
+        }else{
+            b = mid+1;
+        }
+    }
+    return b;
+}
+
+ll rangeSum( ll s){
+    int v = findPos(s);
+    ll sum = 0;
+    v--;
+    int l = leaf(1);
+    int r = leaf(v);
+    while(parent(l) != parent(r)){
+        if(left(parent(l)) == l){
+            sum += tree[right(parent(l))].second;
+        }
+        if(right(parent(r)) == r){
+            sum += tree[left(parent(r))].second;
+        }
+        l = parent(l);
+        r = parent(r);
+    }
+    l = leaf(v+1);
+    r = leaf(m);
+    while(parent(l) != parent(r)){
+        if(left(parent(l)) == l){
+            sum += (ll)tree[right(parent(l))].first * s;
+        }
+        if(right(parent(r)) == r){
+            sum += (ll)tree[left(parent(r))].first * s;
+        }
+        l = parent(l);
+        r = parent(r);
+    }
+    return sum;
 }
 
 vector<bool> solve(){
+    //prepare the tree
+    for(int i =0 ; i<m; i++){
+        val[i] = request[i].second.second;
+    }
+    
+    while(1<<depth < m){
+        depth++;
+        R += R*2;
+    }
 
-}
+    //procesing requests
+    vector<bool> ans;
+
+    for(int i = 0; i<m; i++){
+        if(request[i].first == 'U'){
+            pair<int,ll> cur = request[i].second;
+
+            if(license[cur.first] != 0){
+                update(findPos(license[cur.first]), MP(-1, -license[cur.first]));
+            }
+            license[cur.first] = cur.second;
+            update(findPos(license[cur.first]), MP(1, license[cur.first]));
+        }else if(request[i].first == 'Z'){
+            pair<int,ll> cur = request[i].second;
+
+            ll sum = rangeSum(cur.second);
+            if(sum >= (ll)cur.first * cur.second ){
+                ans.PB(true);
+            }else{
+                ans.PB(false);
+            }
+        }
+    }
+    return ans;
+}   
 
 int main()
 {
