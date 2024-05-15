f4aebaf8ce405bc0971b2bb2215c5487e4c94fa8
TheLordOfCat <franciszekjarz@gmail.com>
2024-05-15 06:46:59 +0200

    RAJ brute

--- 21st/stage2/raj/tester.cpp
+++ 21st/stage2/raj/tester.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 #define MP make_pair
 #define PB push_back
 #define PII pair<int,int>
+
+const int MAXN = 500'000;
+const int MAXM = 1'000'000;
+const int INF = 2'000'000'000;
 
 int n, m;
 vector<vector<int>> graph;
@@ -24,7 +29,7 @@ void getRandom(){
     srand(time(0));
     graph.clear();
     graph.assign(n+1, vector<int>());
-    
+
     n = rand()%10+1;
     m = 0;
     for(int i  = 1; i<=n; i++){
@@ -53,11 +58,48 @@ void printData(){
 }
 
 PII brute(){
+    //making reverse graph
+    vector<vector<int>> graphR(n+1, vector<int>());
+    for(int i = 1; i<=n; i++){
+        for(int j = 0; j<graph[i].size(); j++){
+            graphR[graph[i][j]].PB(i);
+        }
+    }
 
+    //iterating through each edge
+    int ansLen = INF;
+    int ansVec = -1;
+    for(int i = 1; i<=n; i++){
+        queue<int> Q;
+        vector<int> dp(n+1, 0);
+        for(int o = 1; o<=n;o++){
+            if(graph[o].size() == 0){
+                Q.push(o);
+            }
+        }
+
+        while(!Q.empty()){
+            int v = Q.front();
+            if(dp[v] > ansLen){
+                ansLen = dp[v];
+                ansVec = i;
+            }
+            Q.pop();
+
+            for(int o = 0; o<graphR[v].size(); o++){
+                if(!(graphR[v][o] != i)){
+                    Q.push(graphR[v][o]);
+                    dp[graph[v][o]] = max(dp[graph[v][o]], dp[v]+1);
+                }
+            }
+        }
+    }
+
+    return MP(ansVec, ansLen);    
 }
 
 PII solve(){
-
+    
 }
 
 int main()
