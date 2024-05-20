e43072352f6019a3e91b35653048df40ebfe33fd
TheLordOfCat <franciszekjarz@gmail.com>
2024-05-20 17:42:22 +0200

    PRZ coded

--- 21st/stage2/prz/final.cpp
+++ 21st/stage2/prz/final.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include <vector>
+#include <algorithm>
+#include <tuple>
 
 using namespace std;
 
 #define PB push_back
 #define MP make_pair
 #define PII pair<int,int>
+
+const int INF = 2'000'000'000;
 
 int n, k;
 vector<int> c;
@@ -33,34 +37,113 @@ void getData(){
     }
 }
 
+bool customPiarCompare(PII a, PII b){
+    return a.first < b.first;
+}
+
+bool customTupleCompare(tuple<int,int,int> a, tuple<int,int,int> b){
+    return get<1>(a) < get<1>(b);
+}
+
 vector<int> solve(){
-    int left = 1;
-    int right = n-2;
-    
-    for(int i = 0; i<m-1; i++){
-        if(c[left] != x[i]){
-            left++;
-            i--;
-        }
-        if(left >= n){
-            break;
-        }
+    vector<tuple<int,int,int>> seg;
+    //left
+    vector<int> next(n+1, 0);
+    vector<PII> begin(k+1, MP(INF,INF));
+
+    for(int i = n-1; i>= 0; i--){
+        next[i] = begin[c[i]].first;
+        begin[c[i]].first = i;
     }
-    for(int i = 0; i<l-1; i++){
-        if(c[right] != y[i]){
-            right--;
-            i--;
-        }
-        if(right < 0){
-            break;
-        }
+
+    vector<int> pos(k+1, 0);
+    for(int i = 1; i<=k; i++){
+        pos[i] = begin[i].first;
+        begin[i].second = i;
     }
+
+    sort(begin.begin(), begin.end(), customPiarCompare);
+
+    for(int i = 0; i<k; i++){
+        int limit = begin[i].first;
+        if(limit == INF) limit--;
+        for(int j = 0; j<x.size(); j++){
+            int cur = x[j];
+            while(pos[cur] <= limit){
+                pos[cur] = next[pos[cur]];
+            }
+            limit = pos[cur];
+            if(pos[cur] == INF){
+                break;
+            }
+            if(limit == INF) limit--;
+        }
+        seg.PB(make_tuple(limit, 0, begin[i].second));
+    }
+
+    //right
+    next.clear();
+    begin.clear();
+    next.assign(n+1, -INF);
+    begin.assign(k+1, MP(-INF,-INF));
+
+    for(int i = 0; i< n; i++){
+        next[i] = begin[c[i]].first;
+        begin[c[i]].first = i;
+    }
+
+    pos.clear();
+    pos.assign(k+1, 0);
+    for(int i = 1; i<=k; i++){
+        pos[i] = begin[i].first;
+        begin[i].second = i;
+    }
+    sort(begin.begin()+1, begin.end());
+
+    for(int i = k-1; i>=0; i--){
+        int limit = begin[i].first;
+        if(limit == -INF) limit++;
+        for(int j = 0; j<y.size(); j++){
+            int cur = y[j];
+            while(pos[cur] >= limit){
+                pos[cur] = next[pos[cur]];
+            }
+            limit = pos[cur];
+            if(pos[cur] == -1){
+                break;
+            }
+            if(limit == -INF) limit++;
+        }
+        seg.PB(make_tuple(limit,1, begin[i].second));
+    }
+
+    //combine
+    sort(seg.begin(),seg.end());
+
     vector<int> ans;
-    for(int i = left; i<=right; i++){
-        if(c[i] == x[m-1]){
+    vector<bool> started(k+1, false);
+    int ind = 0;
+    int active = 0;
+    for(int i = 0; i<n; i++){
+        while(get<0>(seg[ind]) <= i){
+            if(get<1>(seg[ind]) == 1){
+                if(!started[get<2>(seg[ind])]){
+                    active--;
+                }
+                started[get<2>(seg[ind])] = true;
+            }else{
+                if(!started[get<2>(seg[ind])]){
+                    active++;
+                }
+                started[get<2>(seg[ind])] = true;
+            }
+            ind++;
+        }
+        if(active > 0 && c[i] == x.back()){
             ans.PB(i+1);
         }
     }
+
     return ans;
 }
 
