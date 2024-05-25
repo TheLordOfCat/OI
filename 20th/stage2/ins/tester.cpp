82287889f96479e12f4057e762bfc8ff834a16a7
TheLordOfCat <franciszekjarz@gmail.com>
2024-05-25 08:03:20 +0200

    INS coding

--- 20th/stage2/ins/tester.cpp
+++ 20th/stage2/ins/tester.cpp
@@ -4,16 +4,20 @@
 #include <cstdlib>
 #include <ctime>
 
-
 using namespace std;
 
 #define MP make_pair
 #define PII pair<int,int>
 #define PB push_back
 
+const int INF = 2'000'000'000;
+const int MAXZ = 50;
+const int MAXN = 100'000;
+const int MAXM = 100'000;
+
 int z;
 int n,m;
-vector<vector<int>> logs;
+vector<vector<int>> logs;//t - j - i
 
 void getData(){
     logs.clear();
@@ -49,11 +53,63 @@ void printData(){
     }
 }
 
-vector<int> brute(){
+int brute(){
+    bool ok = true;
+    int ans = 0;
+    for(int i = 0; i<m; i++){
+        int home = n, before = 0, work = 0, after = 0, gone = 0;
+        vector<pair<PII,int>> attend(m+1, MP(MP(0,0),INF));
+        vector<PII> range(n+1, MP(INF,INF));
+        for(int j = 0; j<=i; j++){
+            int t = logs[j][0];
+            int p = logs[j][1];
+            if(attend[t].second == logs[j][2]+1 || attend[t].second == INF){
+                attend[t].second = logs[j][2]+1;
+                if(range[p].first > t){
+                    if(range[p].first != INF){
+                        attend[range[p].first].first.first--;
+                    }
+                    attend[t].first.first++;
+                    range[p].first = t;
+                }
+                if(range[p].second < t){
+                    if(range[p].second != INF){
+                        attend[range[p].second].first.second--;
+                    }
+                    attend[t].first.second++;
+                    range[p].second = t;
+                }
+            }else{
+                ok = false;
+                break;
+            }
+        }
+        for(int j = 1; j<=m; j++){
+            int curWork = before+ work+after;
+            if(curWork > attend[j].second){
 
+            }else if(curWork <attend[j].second){
+                int dif = attend[j].second- curWork;
+                if(dif > home){
+                    ok =false;
+                    break;
+                }else{
+                    home -= dif;
+                    before += dif;
+                }
+            }
+        }
+
+        if(ok){
+            ans++;
+        }else{
+            break;
+        }
+    }
+    return ans;
 }
 
-vector<int> solve(){
+int solve(){
 
 }
 
@@ -63,7 +119,7 @@ int main()
     cin.tie(NULL);
 
     // cin>>z;
-    // z= 1;
+    z= 1;
     for(int i =0; i<z; i++){
         int op = 1;
         for(int test = 1; test<=1; test++){
@@ -73,21 +129,13 @@ int main()
             }else{
                 getRandom();
             }
-            vector<int> ansB = brute();
-            vector<int> ansS = solve();
-            for(int i =0 ; i<ansB.size(); i++){
-                if(ansB[i] != ansS[i]){
-                    cout<<"ERROR\n";
-                    cout<<"BRUTE: ";
-                    for(int i =0; i<ansB.size(); i++){
-                        cout<<ansB[i]<<" ";
-                    }
-                    cout<<"SOLVE: ";
-                    for(int i =0; i<ansS.size(); i++){
-                        cout<<ansS[i]<<" ";
-                    }
-                    return 0;
-                }
+            int ansB = brute();
+            int ansS = solve();
+            if(ansB != ansS){
+                cout<<"ERROR\n";
+                cout<<"BRUTE: "<<ansB<<"\n";
+                cout<<"SOLVE: "<<ansS<<"\n";
+                return 0;
             }
             cout<<"CORRECT\n";
         }
