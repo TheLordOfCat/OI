2d63abaa463948e5fa047908931aea42b38ef16e
TheLordOfCat <franciszekjarz@gmail.com>
2024-06-03 08:48:13 +0200

    BON brute

--- 19th/stage2/bon/tester.cpp
+++ 19th/stage2/bon/tester.cpp
@@ -1,10 +1,130 @@
 #include<iostream>
+#include <vector>
+
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+#define MP make_pair
+#define PB push_back
+#define PII pair<int,int>
+
+int m;
+vector<int> b;
+int n;
+vector<int> a;
+
+void getData(){
+    cin>>m;
+    for(int i =0; i<m; i++){
+        int temp;
+        cin>>temp;
+        b.PB(temp);
+    }
+    cin>>n;
+    for(int i =0; i<n; i++){
+        int temp;
+        cin>>temp;
+        a.PB(temp);
+    }
+}
+
+void getRandom(){
+    srand(time(0));
+    m =rand()%6+1;
+    int len = 20;
+    vector<bool> bon(len+1, false);
+    for(int i =0; i<m; i++){
+        int a = rand()%len +1;
+        while(bon[a]){
+            a = rand()%len +1;
+        }
+        bon[a] = true;
+        b.PB(a);
+    }
+    n = rand()%5+1;
+    for(int i =0; i<n; i++){
+        int temp = rand()%10+1;
+        a.PB(temp);
+    }
+}
+
+void printData(){
+    cout<<"DATA:\n";
+    cout<<m<<"\n";
+    for(int i = 0; i<m; i++){
+        cout<<b[i]<<"\n";
+    }
+    cout<<n<<"\n";
+    for(int i = 0; i<n; i++){
+        cout<<a[i]<<"\n";
+    }
+}
+
+vector<int> brute(){
+    int maxB = -1;
+    for(int i = 0; i<m; i++){
+        maxB = max(maxB, b[i]);
+    }
+
+    vector<int> ans;
+
+    vector<bool> bon(maxB+1, false);
+    vector<bool> vis(maxB+1, false);
+    for(int i = 0; i<n; i++){
+        int cur = a[i];
+        int count = cur;
+        int ind = cur;
+        while(count > 0 && ind <= maxB){
+            if(!vis[ind]){
+                count--;
+                vis[ind] = true;
+                if(bon[ind]){
+                    ans.PB(ind);
+                }
+            }
+            ind += cur;
+        }
+    }
+
+    return ans;
+}
+
+vector<int> solve(){
+    
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int op = 1;
+    for(int test =1; test<=1; test++){
+        if(op == 1){
+            getData();
+        }else{
+            getRandom();
+        }
+        vector<int> ansB = brute();
+        vector<int> ansS = solve();
+        if(ansB.size() != ansS.size()){
+            cout<<"ERROR\n";
+            cout<<"BRUTE: "<<ansB.size()<<"\n";
+            for(int i =0 ;i<ansB.size(); i++){
+                cout<<ansB[i]<<" ";
+            }
+            cout<<"\n";
+            cout<<"SOLVE: "<<ansS.size()<<"\n";
+            for(int i =0 ;i<ansS.size(); i++){
+                cout<<ansS[i]<<" ";
+            }
+            printData();
+            return 0;
+        }
+        cout<<"CORRECT\n";
+    }
 
     return 0;
 }
