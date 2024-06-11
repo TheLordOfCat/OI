933f25a0846ad25fd63c19d15bbe84a67724239f
TheLordOfCat <franciszekjarz@gmail.com>
2024-06-11 06:42:41 +0200

    ROZ done

--- 19th/stage2/roz/final.cpp
+++ 19th/stage2/roz/final.cpp
@@ -1,10 +1,69 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+ull k;
+
+void getData(){
+    cin>>k;
+}
+
+vector<ull> fibonaci(ull l){
+    vector<ull> ans;
+    ans.PB(0);
+    ans.PB(1);
+
+    ull cur = 0+1;
+    while(cur <= l){
+        cur = ans[ans.size()-1] + ans[ans.size()-2];
+        ans.PB(cur);
+    }
+
+    return ans;
+}
+
+int solve(){
+    vector<ull> fib = fibonaci(k);
+
+    ull ans = 0;
+    ull cur = k;
+    while(cur != 0){
+        auto itr = lower_bound(fib.begin(),fib.end(), cur);
+        ull m = itr - fib.begin()-1;
+
+        if(cur - fib[m] < fib[m+1] - cur){
+            ans++;
+            cur -= fib[m];
+        }else{
+            ans++;
+            cur = fib[m+1] - cur;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int p;
+    cin>>p;
+    for(int i = 0; i<p; i++){
+        getData();
+
+        ull ansS = solve();
+        cout<<ansS<<"\n";
+    }
     return 0;
 }
--- 19th/stage2/roz/tester.cpp
+++ 19th/stage2/roz/tester.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include <cstdlib>
 #include <ctime>
@@ -9,6 +10,8 @@ using namespace std;
 #define MP make_pair
 #define PII pair<int,int>
 #define PB push_back
+
+using ull = unsigned long long int;
 
 int k;
 
@@ -22,15 +25,72 @@ void getRandom(){
 }
 
 void printData(){
-    cout<<"DATA: "<<k<<"\n";
+    cout<<"\nDATA: "<<k<<"\n";
+}
+
+vector<int> fibonaci(int l){
+    vector<int> ans;
+    ans.PB(0);
+    ans.PB(1);
+
+    int cur = 0+1;
+    while(cur <= l){
+        cur = ans[ans.size()-1] + ans[ans.size()-2];
+        ans.PB(cur);
+    }
+
+    return ans;
 }
 
 int brute(){
+    vector<int> fib = fibonaci(k);
 
+    int ans = 0;
+    int cur = k;
+    while(cur != 0){
+        int m = 0;
+        for(int i = 0; i<fib.size(); i++){
+            if(fib[i+1] > cur){
+                m = i;
+                break;
+            }
+        }
+
+        if(cur - fib[m] < fib[m+1] - cur){
+            ans++;
+            cur -= fib[m];
+        }else{
+            ans++;
+            cur -= fib[m+1];
+        }
+
+        if(cur < 0 ){
+            cur *= (-1);
+        }
+    }
+
+    return ans;
 }
 
 int solve(){
+    vector<int> fib = fibonaci(k);
 
+    int ans = 0;
+    int cur = k;
+    while(cur != 0){
+        auto itr = lower_bound(fib.begin(),fib.end(), cur);
+        int m = itr - fib.begin()-1;
+
+        if(cur - fib[m] < fib[m+1] - cur){
+            ans++;
+            cur -= fib[m];
+        }else{
+            ans++;
+            cur = fib[m+1] - cur;
+        }
+    }
+
+    return ans;
 }
 
 int main()
@@ -38,12 +98,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int op = 1;
+    int op = 0;
 
     int p = 1;
     // cin>>p;
     for(int i = 0; i<p; i++){
-        for(int test =1 ;test<=1; test++){
+        for(int test =1 ;test<=100000; test++){
             cout<<"TEST nr."<<test<<" = ";
             if(op == 1){
                 getData();
