f6cda72e11155734e39da22df6ff348879fcda43
TheLordOfCat <franciszekjarz@gmail.com>
2024-06-11 19:45:18 +0200

    SEJ base

--- 18th/stage2/sej/tester.cpp
+++ 18th/stage2/sej/tester.cpp
@@ -1,10 +1,82 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+#define MP make_pair
+#define PB push_back
+#define PII pair<int,int>
+
+int n, k;
+vector<int> m;
+
+void getData(){
+    cin>>n>>k;
+    for(int i = 0; i<k; i++){
+        int temp;
+        cin>>temp;
+        m.PB(temp);
+    }
+}
+
+void getRandom(){
+    srand(time(0));
+    n = rand()%50+1;
+    k = rand()%min(n,10)+1;
+    vector<bool> vis(n+1, false);
+    for(int i = 0; i<k; i++){
+        int a = rand()%n+1;
+        while(vis[a]){
+            a = rand()%n+1;
+        }
+        vis[a] = true;
+    }
+}
+
+void printData(){
+    cout<<"DATA: \n";
+    cout<<n<<" "<<k<<"\n";
+    for(int i = 0; i<k; i++){
+        cout<<m[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int brute(){
+
+}
+
+int solve(){
+
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int op = 1;
+    for(int test = 1; test<=1; test++){
+        cout<<"TEST nr."<<test<<" = ";
+        if(op == 1){
+            getData();
+        }else{
+            getRandom();
+        }
+        int ansB = brute();
+        int ansS = solve();
+        if(ansB != ansS){
+            cout<<"ERROR\n";
+            cout<<"BRUTE:"<<ansB<<"\n";
+            cout<<"SOLVE: "<<ansS<<"\n";
+            return 0;
+        }
+        cout<<"CORRECT\n";
+    }
+
 
     return 0;
 }
