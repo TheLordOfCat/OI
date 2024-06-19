f7c104db2a6265e278bacd1060ead2494b6988a8
TheLordOfCat <franciszekjarz@gmail.com>
2024-06-19 08:53:25 +0200

    roz coded

--- 18th/stage2/roz/tester.cpp
+++ 18th/stage2/roz/tester.cpp
@@ -14,6 +14,7 @@ using ull = unsigned long long int;
 #define PB push_back
 
 const int ALPHA = 26;
+const int INF = 2'000'000'000;
 
 int n;
 vector<char> s;
@@ -73,7 +74,42 @@ int brute(){
 }
 
 int solve(){
+    int ans = 0;
 
+    vector<vector<int>> next(26, vector<int>());
+    for(int i = 0; i<n; i++){
+        next[s[i] - 'a'].PB(i);
+    }
+
+    for(int i = 0; i<ALPHA; i++){
+        next[i].PB(INF);
+    }
+
+    for(int l = 0; l<ALPHA; l++){
+        for(int k = 0; k<ALPHA; k++){
+
+            if(next[l].size() > 0 && next[k].size() > 0 ){
+                int sum = 0, mini = INF, lastSum = 0;
+                int len = next[l].size() + next[k].size();                
+                int itrL = 0, itrK = 0;
+
+                for(int  i = 0; i<len; i++){
+                    if(next[k][itrK] > next[l][itrL]){
+                        sum += 1;
+                        itrL++;
+                    }else if(itrK != INF){
+                        sum -= 1;
+                        itrK ++;
+                        mini = min(mini, lastSum);
+                        lastSum = sum;
+                    }
+                    ans = max(ans, sum - mini);
+                }
+            }
+        }
+    }
+
+    return ans;
 }
 
 int main()
