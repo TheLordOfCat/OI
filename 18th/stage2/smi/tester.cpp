d9e9b6b91adbf6078586334ebe3ef573ef3c5303
TheLordOfCat <franciszekjarz@gmail.com>
2024-06-20 09:49:50 +0200

    SMI debugging

--- 18th/stage2/smi/tester.cpp
+++ 18th/stage2/smi/tester.cpp
@@ -20,6 +20,7 @@ vector<vector<pair<int,PII>>> graph;
 
 void getData(){
     cin>>n>>m;
+    graph.assign(n+1, vector<pair<int, PII>>());
     for(int i = 0; i<m; i++){
         int a, b, s, t;
         cin>>a>>b>>s>>t;
@@ -118,11 +119,23 @@ bool verify(vector<vector<int>> v){
     for(int i = 0; i<v.size(); i++){
         v[i].PB(v[i].front());
         for(int j = 0; j<v[i].size()-1; j++){
-            
-            for(int o = 0; o<graphCopy[v[i][j]].size(); o++){
-                int cur = graphCopy[v[i][j]][o].first;
-                if(cur == v[i][j+1]){
-                    graphCopy[v[i][j]][o].second.first = (graphCopy[v[i][j]][o].second.first + 1)%2;
+            int V = v[i][j];
+            int next = v[i][j+1];
+            for(int o = 0; o<graphCopy[V].size(); o++){
+                int cur = graphCopy[V][o].first;
+                if(cur == next){
+                    graphCopy[V][o].second.first = (graphCopy[V][o].second.first + 1)%2;
+                }
+            }
+        }
+        //in reverse order
+        for(int j = v[i].size() -1; j> 0; j--){
+            int V = v[i][j];
+            int next = v[i][j-1];
+            for(int o = 0; o<graphCopy[V].size(); o++){
+                int cur = graphCopy[V][o].first;
+                if(cur == next){
+                    graphCopy[V][o].second.first = (graphCopy[V][o].second.first + 1)%2;
                 }
             }
         }
@@ -160,57 +173,87 @@ bool verify(vector<vector<int>> v){
 vector<vector<int>> solve(){
     vector<vector<int>> ans;
     //create a new graph
-    vector<vector<pair<int,bool>>> graph0(n+1, vector<pair<int,false>>());
+    vector<bool> used(n+1, false);
+    vector<vector<pair<PII,bool>>> graph0(n+1, vector<pair<PII,bool>>());
     for(int i = 1; i<=n; i++){
-        for(int j = 0; j < graph[i].size(); j++){
-            if(graph[i][j].second.first != graph[i][j].second.second){
-                graph0[i].PB(MP(j,false));
-                graph0[j].PB(MP(i,false));
+        if(!used[i]){
+            stack<int> S;
+            S.push(i);
+            while(!S.empty()){
+                int v = S.top();
+                S.pop();
+                used[v] = true;
+                
+                for(int j= 0; j<graph[v].size(); j++){
+                    int cur = graph[v][j].first;
+                    if(!used[cur]){
+                        if(graph[v][j].second.first != graph[v][j].second.second){
+                            graph0[v].PB(MP(MP(cur,graph0[cur].size()), false));
+                            graph0[cur].PB(MP(MP(v,graph0[v].size()-1),false));
+                        }
+                        S.push(cur);
+                    }
+                }
             }
         }
     }
     
     //check if valid 
     for(int i = 1; i<=n; i++){
-        if(graph[i].size()%2 != 0){
+        if(graph0[i].size()%2 != 0){
             return vector<vector<int>>();
         }
     }
 
     //find loops
-    vector<int> vis(n+1, 0);
-    vector<int> pre(n+1, 0);
-    for(int i = 1; i<=n; i++){
-        if(vis[i] != graph0[i].size()){
-            stack<int> S;
-            S.push(i);
-            while(S.empty()){
-                int v = S.top();
-                S.pop();
-                vis[v] = true;
+    vector<bool> vis(n+1, false);
+    vector<int> pre(n+1, -1);
 
-                for(int j = 0; j<graph0[v].size(); j++){
-                    int cur = graph0[v][j].first;
-                    if(!graph0[v][j].second){
-                        graph0[v][j].second = true;
-                        if(vis[graph[v][j].first]){
-                            //loop found
-                            vector<int> temp;
-                            
-                            int end = graph[v][j].first;
-                            int V = v;
-                            while(V != v){
+    for(int i = 1; i<=n; i++){
+        stack<int> S;
+        S.push(i);
+        while(!S.empty()){
+            int v = S.top();
+            S.pop();
+            vis[v] = true;
+
+            for(int j = 0; j<graph0[v].size(); j++){
+                int cur = graph0[v][j].first.first;
+                int rev = graph0[v][j].first.second;
+                int b = graph0[v][j].second;
+                if(!b){
+                    graph0[v][j].second = true;
+                    graph0[cur][rev].second = true;
+
+                    if(vis[cur]){
+                        //found loop
+                        int V = v;
+                        int end = cur;
+                        
+                        vector<int> temp;
+                        while(V != end){
+                            if(V == i){
+                                temp.PB(V);
+                                vis[V] = false;
+                                V = cur;
+                                end = i;
+                            }else{
                                 temp.PB(V);
                                 vis[V] = false;
                                 V = pre[V];
                             }
-
-                            reverse(temp.begin(), temp.end());
-                            ans.PB(temp);
-                        }else{
-                            pre[graph0[v][j].first] = v;
                         }
-                        S.push(graph[v][j].first);
+                        if(vis[cur]){
+                            temp.PB(cur);
+                            vis[cur] = false;
+                        }
+
+                        reverse(temp.begin(), temp.end());
+                        ans.PB(temp);
+                    }else{
+                        S.push(cur);
+                        vis[cur] = true;
+                        pre[cur] = v;
                     }
                 }
             }
@@ -234,22 +277,24 @@ int main()
             getRandom();
         }
         vector<vector<int>> ansS = solve();
-        if(!verify(ansS)){
-            cout<<"ERROR\n";
-            if(ansS.size() == 0){
-                cout<<"NIE\n";
-            }else{
-                cout<<"TAK";
-                for(int i = 0; i<ansS.size(); i++){
-                    cout<<ansS[i].size()<<" ";
-                    for(int j = 0; j<ansS[i].size(); j++){
-                        cout<<ansS[i][j]<<" ";
+        if(ansS.size() != 0){
+            if(!verify(ansS)){
+                cout<<"ERROR\n";
+                if(ansS.size() == 0){
+                    cout<<"NIE\n";
+                }else{
+                    cout<<ansS.size()<<"\n";
+                    for(int i = 0; i<ansS.size(); i++){
+                        cout<<ansS[i].size()<<" ";
+                        for(int j = 0; j<ansS[i].size(); j++){
+                            cout<<ansS[i][j]<<" ";
+                        }
+                        cout<<ansS[i].front()<<"\n";
                     }
-                    cout<<ansS[i].front()<<"\n";
                 }
+                printData();
+                return 0;
             }
-            printData();
-            return 0;
         }
         cout<<"CORRECT\n";
     }
