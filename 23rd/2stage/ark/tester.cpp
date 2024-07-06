c5af9800857e910c71f5969f569ea1d3dc37f92a
TheLordOfCat <franciszekjarz@gmail.com>
2024-07-06 07:53:00 +0200

    ARK brute done

--- 23rd/2stage/ark/tester.cpp
+++ 23rd/2stage/ark/tester.cpp
@@ -16,8 +16,28 @@ using ull = unsigned long long int;
 #define PB push_back
 #define MT make_tuple
 
+bool operator==(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+    if (lhs.first == rhs.first && lhs.second == rhs.second) {
+        return true;  
+    }
+    return false;   
+}
+
+PII operator+(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+    return MP(lhs.first + rhs.first, lhs.second + rhs.second);
+}
+
+PII operator+=( pair<int, int>& lhs, const pair<int, int>& rhs) {
+    lhs.first += rhs.first;
+    lhs.second += rhs.second;
+    return lhs;
+}
+
+
 int n, m, k;
 vector<PII> blocks;
+
+int startX, startY;
 
 void getData(){
     cin>>n>>m>>k;
@@ -26,6 +46,8 @@ void getData(){
         cin>>a>>b;
         blocks.PB(MP(a,b));
     }
+    startX = n;
+    startY = 0;
 }
 
 void getRandom(){
@@ -46,6 +68,8 @@ void getRandom(){
         }
         blocks.PB(MP(a,b));
     }
+    startX = n;
+    startY = 0;
 }
 
 void printData(){
@@ -56,8 +80,94 @@ void printData(){
     }
 }
 
+//return center of a block in scaled grid
+PII centerBlock(PII cord){
+    return MP(cord.first - 1, cord.second-1);
+}
+
+//0 - not wall, 1 - side walls, 2 - floors
+int isWall(PII cord){
+    if(cord.first == 0 || cord.first == n){
+        return 1;
+    }
+    if(cord.second == 0 || cord.second == m){
+        return 2;
+    }
+    return 0;
+}
+
+int isBlock(PII cord, PII dir){
+
+    for(int i = 0; i<k; i++){
+        PII cen = centerBlock(blocks[i]);
+        vector<PII> temp;
+        temp.PB(MP(cen.first + 1, cen.second));
+        temp.PB(MP(cen.first - 1, cen.second));
+        temp.PB(MP(cen.first, cen.second + 1));
+        temp.PB(MP(cen.first, cen.second - 1));
+
+        for(int j = 0; j<temp.size(); j++){
+            if(temp[j] == cord){
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
 ull brute(){
-    
+    ull ans = 0;
+    int blocksLeft = k;
+    PII cord = MP(startX, startY);
+    PII dir = MP(-1,1);
+    vector<bool> vis(k+1, false);
+
+    while(blocksLeft > 0){
+        ans++;
+        cord += dir;
+        int w = isWall(cord);
+
+        if(w == 1){
+            dir.first *= -1;
+        }else if(w == 2){
+            dir.second *= -1;
+        }else{
+            int b = isBlock(cord, dir);
+
+            if(b >= 0 && !vis[b]){
+                vis[b] = true;
+                blocksLeft--;
+                if(blocks[b].first-1 == cord.first){
+                    dir.second *= -1;
+                }else if(blocks[b].second-1 == cord.second){
+                    dir.first *= -1;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
+
+PII reflect(int cord, int dir){
+
+}
+
+void mergetPath(PII cord, PII dir){
+
+}
+
+void mergeBlock(){
+
+}
+
+void mergeWall(){
+
+}
+
+void removeBlock(){
+
 }
 
 ull solve(){
@@ -75,6 +185,14 @@ int main() {
         }else{
             getRandom();
         }
+
+        for(int i = 0; i<k; i++){
+            blocks[i].first *= 2;
+            blocks[i].second *= 2;
+        }
+        n *= 2;
+        m *= 2;
+
         ull ansB = brute();
         ull ansS = solve();
         if(ansB != ansS){
