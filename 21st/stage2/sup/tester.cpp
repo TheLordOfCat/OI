b391851c5ac67ee3675609fb674e54404ab7cf01
TheLordOfCat <franciszekjarz@gmail.com>
2024-07-17 07:39:36 +0200

    SUP base

--- 21st/stage2/sup/tester.cpp
+++ 21st/stage2/sup/tester.cpp
@@ -1,7 +1,5 @@
-#include<iostream>
-#include<vector>
-#include <stack>
-#include <queue>
+#include <iostream>
+#include <vector>
 
 #include <cstdlib>
 #include <ctime>
@@ -9,171 +7,66 @@
 using namespace std;
 
 #define MP make_pair
-#define PB push_back
 #define PII pair<int,int>
+#define PB push_back
+
+using ll = long long int;
+using ull = unsigned long long int;
 
 int n, q;
-vector<int> parent;
 vector<int> k;
+vector<int> a; 
 
 void getData(){
     cin>>n>>q;
-
-    for(int i = 0; i<q; i++){
+    for(int i = 0; i <q; i++){
         int temp;
         cin>>temp;
         k.PB(temp);
     }
-
-    parent.PB(-1);
-    parent.PB(-1);
-    for(int i = 2; i<=n; i++){
+    for(int i =0; i<n; i++){
         int temp;
         cin>>temp;
-        parent.PB(temp);
+        a.PB(temp);
     }
 }
 
 void getRandom(){
     srand(time(0));
+
     n = rand()%10+1;
-    q = rand()%10+1;
+    q = rand()%3+1;
+
     for(int i = 0; i<q; i++){
         int temp = rand()%10+1;
         k.PB(temp);
     }
-    parent.clear();
-    parent.PB(-1);
-    parent.PB(-1);
-    for(int i = 2; i<=n; i++){
-        int temp = rand()%i+1;
-        if(temp == i) temp--;
-        parent.PB(temp);
+    
+    for(int i = 0; i<n; i++){
+        int temp = rand()%10+1;
+        a.PB(temp);
     }
 }
 
 void printData(){
+    cout<<"\nDATA: \n";
     cout<<n<<" "<<q<<"\n";
     for(int i = 0; i<q; i++){
         cout<<k[i]<<" ";
     }
     cout<<"\n";
-    for(int i = 2;i<=n; i++){
-        cout<<parent[i]<<" ";
+    for(int i = 0; i<n; i++){
+        cout<<a[i]<<" ";
     }
     cout<<"\n";
 }
 
-struct CustomComparator {
-    bool operator()(const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
-        if (p1.second == p2.second) {
-            return p1.first > p2.first;
-        }
-        return p1.second < p2.second;
-    }
-};
-
 vector<int> brute(){
-    vector<vector<int>> graph(n+1, vector<int>());
-    for(int i = 2; i<=n; i++){
-        graph[parent[i]].PB(i);
-    }
-
-    vector<int> maxDepth(n+1,0);
-    stack<pair<int,bool>> S;
-    S.push(MP(1,false));
-
-    //filing maxDepth
-    while(!S.empty()){
-        int v = S.top().first;
-        bool b = S.top().second;
-        S.pop();
-        if(b){
-            for(int i = 0; i<graph[v].size(); i++){
-                maxDepth[v] = max(maxDepth[v], maxDepth[graph[v][i]]+1);
-            }
-        }else{
-            S.push(MP(v,true));
-            for(int i = 0; i<graph[v].size(); i++){
-                S.push(MP(graph[v][i],false));
-            }
-        }
-    }
-
-    //simulating
-    vector<int> ans;
-    for(int i =0 ; i<q; i++){
-        int turns = 0;
-        int left = n;
-        priority_queue<PII> Q;
-        Q.push(MP(1,maxDepth[1]));
-
-        while(left > 0){
-            vector<int> next;
-            for(int j = 0; j<k[i]; j++){
-                if(!Q.empty()){
-                    next.PB(Q.top().first);
-                    Q.pop();
-                    left--;
-                }else{
-                    break;
-                }
-            }
-            turns++;
-            for(int j = 0; j<next.size(); j++){
-                for(int o = 0; o <graph[next[j]].size(); o++){
-                    int cur = graph[next[j]][o];
-                    Q.push(MP(cur, maxDepth[cur]));
-                }
-            }
-        }
-        ans.PB(turns);
-    }
-
-    return ans;
+    return vector<int>();
 }
 
-struct block{
-    int index;
-    int size;
-    int height;
-    block(int i1, int s1 ,int h1){
-        index = i1;
-        size = s1;
-        height = h1;
-    }
-    block(){
-        index = 0;
-        size = 0;
-        height = 0;
-    }
-};
-
 vector<int> solve(){
-    vector<int> time(n+1, 0);
-    vector<vector<block>> blocksVec(2, vector<block>());
-    for(int i = n-1; i>0; i--){
-        int exces = 0;
-        blocksVec[i%2].clear()
-        for(int j = 0; j<blocksVec[(i+1)%2].size(); j++){
-            block b = blocksVec[(i+1)%2][j];
-            if(b.height > i){
-                exces += (b.height-i)*b.size;
-                blocksVec.PB(block(b.index, b.size, b.height));
-            }else{
-                exces -= (i-b.height)*b.size;
-                
-            }
-        }   
-    }
-
-    //getting the answer
-    vector<int> ans;
-    for(int i = 0; i<q; i++){
-        ans.PB(time[k[i]]);
-    }
-
-    return ans;
+    return vector<int>();
 }
 
 int main()
@@ -183,7 +76,6 @@ int main()
 
     int op = 1;
     for(int test = 1; test<=1; test++){
-        cout<<"TEST nr."<<test<<" = ";
         if(op == 1){
             getData();
         }else{
@@ -191,9 +83,9 @@ int main()
         }
         vector<int> ansB = brute();
         vector<int> ansS = solve();
-        for(int i = 0; i<ansB.size(); i++){
+        for(int i = 0; i<q; i++){
             if(ansB[i] != ansS[i]){
-                cout<<"ERRROR\n";
+                cout<<"ERROR\n";
                 cout<<"BRUTE: ";
                 for(int j = 0; j<ansB.size(); j++){
                     cout<<ansB[j]<<" ";
@@ -207,7 +99,7 @@ int main()
                 return 0;
             }
         }
-        cout<<"COORECT\n";
+        cout<<"CORRECT\n";
     }
 
     return 0;
