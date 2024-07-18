a06ceafc0229f6ed99e2b1e487022110570d893a
TheLordOfCat <franciszekjarz@gmail.com>
2024-07-18 07:40:42 +0200

    SUP coded

--- 21st/stage2/sup/tester.cpp
+++ 21st/stage2/sup/tester.cpp
@@ -131,7 +131,108 @@ vector<int> brute(){
 }
 
 vector<int> solve(){
-    return vector<int>();
+    vector<int> opsTurns(n+1, 0);
+
+    vector<PII> blocks(n+1, MP(0,0));
+    for(int i = 0; i<n; i++){
+        blocks.PB(MP(a[i], 1));
+    }
+
+    for(int op = n; op >0; op--){
+        int len = 0;
+        vector<PII> nextBlocks;
+
+        int delta = 0;
+        for(int i = 0; i<blocks.size(); i++){
+            if(blocks[i].first >= op){
+                //too many blocks
+                delta += (blocks[i].first - op) * blocks[i].second;
+                if(nextBlocks.size() > 0){
+                    if(nextBlocks.back().first == op){
+                        nextBlocks.back().second += blocks[i].second;
+                    }else{
+                        nextBlocks.PB(MP(op, blocks[i].second));
+                    }
+                }else{
+                    nextBlocks.PB(MP(op, blocks[i].second));
+                }
+
+            }else{
+                //too little blocks
+                int excess = (op - blocks[i].first) * blocks[i].second;
+                if(excess >= delta){
+                    vector<PII> parts;
+
+                    //part 1
+                    int num = delta/(op - blocks[i].first);
+                    parts.PB(MP(op, num));
+                    delta -= (op - blocks[i].first)* num;
+                    // part 2
+                    if(delta > 0) parts.PB(MP(delta, 1));
+                    //part 3
+                    if(delta > 0){
+                        parts.PB(MP(blocks[i].first, blocks[i].second - 1 - num));
+                    }else{
+                        parts.PB(MP(blocks[i].first, blocks[i].second - num));
+                    }
+
+                    if(nextBlocks.size() > 0){
+                        if(nextBlocks[nextBlocks.size()-1].first == parts[0].first){
+                            nextBlocks[nextBlocks.size()-1].second += parts[0].second;
+                        }else{
+                            nextBlocks.PB(MP(op, parts[0].second));
+                        }
+                    }else{
+                        nextBlocks.PB(MP(op, parts[0].second));
+                    }
+
+                    for(int i = 1; i<parts.size(); i++){
+                        nextBlocks.PB(MP(parts[i].first, parts[i].second));
+                    }
+
+                    delta = 0;
+                }else{
+                    if(nextBlocks.size() > 0){
+                        if(nextBlocks.back().first == op){
+                            nextBlocks.back().second += blocks[i].second;
+                        }else{
+                            nextBlocks.PB(MP(op, blocks[i].second));
+                        }
+                    }else{
+                        nextBlocks.PB(MP(op, blocks[i].second));
+                    }
+                    
+                    delta -= excess;
+                }
+            }
+            len += blocks[i].second;
+        }
+
+        if(delta > 0){
+            vector<PII> parts;
+            //part 1
+            int num = delta/op;
+            nextBlocks.back().second += num;
+
+            len += num;
+            delta -= op*num;
+            // part 2
+            if(delta > 0){
+                nextBlocks.PB(MP(delta, 1));
+                len += 1;
+            }
+        }
+
+        blocks = nextBlocks;
+        opsTurns[op] = len; 
+    }
+
+    vector<int> ans;
+    for(int i = 0; i<q; i++){
+        ans.PB(opsTurns[k[i]]);
+    }
+
+    return ans;
 }
 
 int main()
