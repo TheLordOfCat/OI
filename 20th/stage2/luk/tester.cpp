627d2dbbc43f935e29f674b404a380773cb5367c
TheLordOfCat <franciszekjarz@gmail.com>
2024-07-27 09:49:45 +0200

    LUK coded

--- 20th/stage2/luk/tester.cpp
+++ 20th/stage2/luk/tester.cpp
@@ -123,8 +123,68 @@ int brute(){
     return ans;
 }
 
-int solve(){
+bool check(int k, vector<vector<int>>& graph){
+    vector<int> dp(n+1, 0);
+    vector<int> parent(n+1, -1);
 
+    stack<pair<int,bool>> S;
+    S.push(MP(1,false));
+    while(!S.empty()){
+        int v = S.top().first;
+        bool b = S.top().second;
+        S.pop();
+
+        if(b){
+            int sum = 0;
+            for(int j = 0; j<graph[v].size(); j++){
+                int cur = graph[v][j];
+                sum += dp[cur];
+            }
+            dp[v] = max(0,sum+1-k);
+            continue;
+        }
+
+        S.push(MP(v,true));
+        for(int j = 0; j<graph[v].size(); j++){
+            int cur = graph[v][j];
+            if(parent[cur] == -1){
+                parent[cur] = v;
+                S.push(MP(cur, false));
+            }
+        }
+    }
+
+    if(dp[1] == 0){
+        return true;
+    }
+
+    return false;
+}
+
+int solve(){
+    vector<vector<int>> graph(n+1, vector<int>());
+
+    for(int i = 0; i<edges.size(); i++){
+        int a = edges[i].first;
+        int b = edges[i].second;
+        graph[a].PB(b);
+        graph[b].PB(a);
+    }
+
+    int ans = n;
+    int l =1;
+    int r = n;
+    while(l<r){
+        int mid = (l+r)/2;
+        if(check(mid, graph)){
+            ans = mid;
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+    }
+
+    return ans;
 }
 
 int main(){
