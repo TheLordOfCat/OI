445a30327d90aa62f365a81ed7cb11753009e5a9
TheLordOfCat <franciszekjarz@gmail.com>
2024-07-30 07:30:22 +0200

    MOR

--- 20th/stage2/mor/final.cpp
+++ 20th/stage2/mor/final.cpp
@@ -37,6 +37,10 @@ void getData(){
     }
 }
 
+bool comp(const PII a, const PII b){
+    return a.first< b.first;
+}
+
 vector<int> solve(){
     //create graph
     vector<vector<int>> graph(n+1, vector<int>());
@@ -47,7 +51,8 @@ vector<int> solve(){
         graph[b].PB(a);
     }
 
-    //traverse the graph
+    /*
+    traverse the graph
     vector<vector<PII>> shortest(n+1,vector<PII>(n+1, MP(INF,INF)));
 
     for(int i = 1; i<=n; i++){
@@ -92,6 +97,67 @@ vector<int> solve(){
             }
         }
         ans.PB(ok);
+    }
+    */
+
+    vector<PII> quests;
+    for(int i = 0; i<k; i++) quests.PB(MP(get<0>(requests[i]),i));
+
+    sort(quests.begin(), quests.end());
+
+    vector<int> ans(k, -1);
+    vector<PII> shortest(n+1, MP(INF,INF));
+
+    for(int i = 0; i<k; i++){
+        int s = get<0>(requests[quests[i].second]), t = get<1>(requests[quests[i].second]), d = get<2>(requests[quests[i].second]); 
+
+        bool up = true;
+        if(i > 1){
+            if(quests[i-1].first == quests[i].first){
+                up = false;
+            }
+        }
+        
+        if(up){
+            shortest.clear();
+            shortest.assign(n+1, MP(INF,INF));
+
+            queue<PII> Q;
+            Q.push(MP(s,0));
+
+            while(!Q.empty()){
+                int v = Q.front().first;
+                int l = Q.front().second;
+                Q.pop();
+
+                for(int j = 0; j<graph[v].size(); j++){
+                    int cur = graph[v][j];
+                    if((l+1)%2 == 0){
+                        if(shortest[cur].first >= l+1){
+                            shortest[cur].first = l+1;
+                            Q.push(MP(cur,l+1));
+                        }
+                    }else{
+                        if(shortest[cur].second >= l+1){
+                            shortest[cur].second = l+1;
+                            Q.push(MP(cur,l+1));
+                        }
+                    }
+                }
+            }
+        }
+
+        int ok = 0;
+        if(d%2 == 0){
+            if(shortest[t].first <= d){
+                ok = 1;
+            }
+        }else{
+            if(shortest[t].second <= d){
+                ok = 1;
+            }
+        }
+        ans[quests[i].second] = ok;
     }
 
     return ans;
--- 20th/stage2/mor/tester.cpp
+++ 20th/stage2/mor/tester.cpp
@@ -115,6 +115,10 @@ vector<int> brute(){
     return ans;
 }
 
+bool comp(const PII a, const PII b){
+    return a.first< b.first;
+}
+
 vector<int> solve(){
     //create graph
     vector<vector<int>> graph(n+1, vector<int>());
@@ -125,7 +129,8 @@ vector<int> solve(){
         graph[b].PB(a);
     }
 
-    //traverse the graph
+    /*
+    traverse the graph
     vector<vector<PII>> shortest(n+1,vector<PII>(n+1, MP(INF,INF)));
 
     for(int i = 1; i<=n; i++){
@@ -166,6 +171,67 @@ vector<int> solve(){
             }
         }else{
             if(shortest[s][t].second <= d){
+                ok = 1;
+            }
+        }
+        ans.PB(ok);
+    }
+    */
+
+    vector<PII> quests;
+    for(int i = 0; i<k; i++) quests.PB(MP(get<0>(requests[i]),i));
+
+    sort(quests.begin(), quests.end());
+
+    vector<int> ans(k, -1);
+    vector<PII> shortest(n+1, MP(INF,INF));
+
+    for(int i = 0; i<k; i++){
+        int s = get<0>(requests[i]), t = get<1>(requests[i]), d = get<2>(requests[i]); 
+
+        bool up = true;
+        if(i > 1){
+            if(quests[i-1].first == quests[i].first){
+                up = false;
+            }
+        }
+        
+        if(up){
+            shortest.clear();
+            shortest.assign(n+1, MP(INF,INF));
+
+            queue<PII> Q;
+            Q.push(MP(s,0));
+
+            while(!Q.empty()){
+                int v = Q.front().first;
+                int l = Q.front().second;
+                Q.pop();
+
+                for(int j = 0; j<graph[v].size(); j++){
+                    int cur = graph[v][j];
+                    if((l+1)%2 == 0){
+                        if(shortest[cur].first >= l+1){
+                            shortest[cur].first = l+1;
+                            Q.push(MP(cur,l+1));
+                        }
+                    }else{
+                        if(shortest[cur].second >= l+1){
+                            shortest[cur].second = l+1;
+                            Q.push(MP(cur,l+1));
+                        }
+                    }
+                }
+            }
+        }
+
+        int ok = 0;
+        if(d%2 == 0){
+            if(shortest[t].first <= d){
+                ok = 1;
+            }
+        }else{
+            if(shortest[t].second <= d){
                 ok = 1;
             }
         }
