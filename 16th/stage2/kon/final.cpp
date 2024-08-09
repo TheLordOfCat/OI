49103ea90a4ea71356b5b018b8bcf30ebfbcf3b1
TheLordOfCat <franciszekjarz@gmail.com>
2024-08-09 09:42:48 +0200

    KON coded

--- 16th/stage2/kon/final.cpp
+++ 16th/stage2/kon/final.cpp
@@ -1,10 +1,102 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+int n, k;
+vector<vector<int>> passen;
+
+void getData(){
+    cin>>n>>k;
+    for(int i =1; i<n; i++){
+        vector<int> vec;
+        for(int j =1; j <=n-i; j++){
+            int temp;
+            cin>>temp;
+            vec.PB(temp);
+        }
+        passen.PB(vec);
+    }
+}
+
+pair<int,vector<int>> solve(){
+    //creating sum table
+    vector<vector<int>> pas(n, vector<int>(n, 0));
+    for(int i = 0; i<n; i++){
+        for(int j =0; j<n-i-1; j++){
+            pas[i+1][n-i-1-j] = passen[i][j];
+        }
+    }
+    vector<vector<int>> S(n, vector<int>(n, 0));
+
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<n; j++){
+            S[i][j] = pas[i][j] + S[i-1][j] + S[i][j-1] - S[i-1][j-1];
+        }
+    }
+
+    //dynamic programming the answer
+    vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
+    vector<vector<int>> pre(n+1, vector<int>(k+1, 0));
+
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<=k; j++){
+
+            for(int o = i; o>=0; o--){
+                int sum = S[i][n-i]-S[o][n-i];
+                if(dp[i][j] < dp[o][j-1] + sum){
+                    pre[i][j] = o;
+                    dp[i][j] = dp[o][j-1] + sum;
+                }
+            }
+
+        }
+    }
+
+    int best = k;
+    for(int i = k+1; i<n; i++){
+        if(dp[i][k] > dp[best][k]){
+            best = i;
+        }
+    }
+
+    vector<int> ans;
+    int ind = best;
+    int t = k;
+    while(ind != 0){
+        ans.PB(ind);
+        ind = pre[ind][t];
+        t--;
+    }
+    reverse(ans.begin(),ans.end());
+
+    return MP(dp[best][k], ans);
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    getData();
+
+    pair<int,vector<int>> ansS = solve();
+    for(int j = 0; j<ansS.second.size(); j++){
+        cout<<ansS.second[j]<<" ";
+    }
+    cout<<"\n";
 
     return 0;
 }
--- 16th/stage2/kon/tester.cpp
+++ 16th/stage2/kon/tester.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -99,8 +103,8 @@ pair<int,vector<int>> solve(){
     //creating sum table
     vector<vector<int>> pas(n, vector<int>(n, 0));
     for(int i = 0; i<n; i++){
-        for(int j =0; j<n; j++){
-            pas[i+1][j+1] = passen[i][n-1-j];
+        for(int j =0; j<n-i-1; j++){
+            pas[i+1][n-i-1-j] = passen[i][j];
         }
     }
     vector<vector<int>> S(n, vector<int>(n, 0));
@@ -115,11 +119,11 @@ pair<int,vector<int>> solve(){
     vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
     vector<vector<int>> pre(n+1, vector<int>(k+1, 0));
 
-    for(int i = 1; i<=n; i++){
+    for(int i = 1; i<n; i++){
         for(int j = 1; j<=k; j++){
 
-            for(int o = i; o>=1; o--){
-                int sum = S[i][i]-S[o][i];
+            for(int o = i; o>=0; o--){
+                int sum = S[i][n-i]-S[o][n-i];
                 if(dp[i][j] < dp[o][j-1] + sum){
                     pre[i][j] = o;
                     dp[i][j] = dp[o][j-1] + sum;
@@ -130,7 +134,7 @@ pair<int,vector<int>> solve(){
     }
 
     int best = k;
-    for(int i = k+1; i<=n; i++){
+    for(int i = k+1; i<n; i++){
         if(dp[i][k] > dp[best][k]){
             best = i;
         }
@@ -138,10 +142,13 @@ pair<int,vector<int>> solve(){
 
     vector<int> ans;
     int ind = best;
+    int t = k;
     while(ind != 0){
         ans.PB(ind);
-        ind = pre[ind][k];
+        ind = pre[ind][t];
+        t--;
     }
+    reverse(ans.begin(),ans.end());
 
     return MP(dp[best][k], ans);
 }
@@ -153,6 +160,7 @@ int main()
 
     int op = 1;
     for(int test = 1; test<=1 ;test++){
+        cout<<"TEST nr."<<test<<" = ";
         if(op == 1){
             getData();
         }else{
@@ -175,7 +183,7 @@ int main()
             cout<<"\n";
             return 0;
         }
-
+        cout<<"CORRECT\n";
     }
 
     return 0;
