033a404d7860ac43b1c954041254f1b40e269501
TheLordOfCat <franciszekjarz@gmail.com>
2024-08-20 08:44:57 +0200

    KON

--- 25th/2stage/kon/tester.cpp
+++ 25th/2stage/kon/tester.cpp
@@ -1,10 +1,152 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+int n, m;
+vector<PII> passengers;
+
+void getData(){
+    passengers.clear();
+
+    cin>>m>>n;
+    for(int i = 0; i<n; i++){
+        int a, b;
+        cin>>a>>b;
+        passengers.PB(MP(a,b));
+    }
+}
+
+void getRandom(){
+    passengers.clear();
+    srand(time(0));
+
+    m = rand()%15+1;
+    n = rand()%5+1;
+
+    for(int i = 0; i<n; i++){
+        int a = -1, b = -1;
+        while(a == b || a == -1|| b == -1){
+            a = rand()%m+1;
+            b = rand()%m+1;
+        }
+        if(a>b) swap(a,b);
+        passengers.PB(MP(a,b));
+    }
+}
+
+void printData(){
+    cout<<"DATA: \n";
+    cout<<n<<" "<<m<<"\n";
+    for(int i =0; i<passengers.size(); i++){
+        cout<<passengers[i].first<<" "<<passengers[i].second<<"\n";
+    }
+}
+
+struct comparePas{
+    bool oparetor(const PII a, const PII b){
+        if(a.first == a.first){
+            return b.first < b.second;
+        }
+        return a.first < a.second;
+    }
+};
+
+PII brute(){
+    ll minOp, comb = 0;
+    vector<PII> pas = passengers;
+    sort(pas.begin(), pas.end());
+
+    for(int o = 1; o <= n; o++){
+
+        vector<int> per;
+        for(int i = 1; i<=n; i++){
+            per.PB(i);
+        }
+        
+        //iterate through combinations
+        do{
+            vector<bool> vis(n+1, false);
+            for(int i= 0; i<o; i++){
+                vis[per[i]] = true;
+            }
+            //verifying
+            int count = 0;
+            for(int i = 0; i<pas.size(); i++){
+                for(int j = pas[i].first; j<=pas[i].second; j++){
+                    if(vis[j]){
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if(count == n){
+                comb++;
+            }
+
+        }while(next_permutation(per.begin(),per.end()));
+
+        if(comb > 0){
+            minOp = o;
+            break;
+        }
+    }   
+
+    pair<int,int> ans = MP(minOp, comb);
+
+    return ans;
+}
+
+PII solve(){
+
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int op = 1;
+    int z = 1;
+    // cin>>z;
+    for(int o = 0; o<z; o++){
+        for(int test = 1; test<=1; test++){
+            cout<<"TEST nr."<<test<<" = ";
+            if(op == 1){
+                getData();
+            }else{
+                getRandom();
+            }
+            PII ansB = brute();
+            PII ansS = solve();
+            if(ansB.first != ansS.first || ansB.second != ansS.second){
+                cout<<"ERROR\n";
+                cout<<"BRUTE: ";
+                cout<<ansB.first<<" "<<ansB.second<<"\n";
+                cout<<"SOLVE: ";
+                cout<<ansS.first<<" "<<ansS.second<<"\n";
+                printData();
+                return 0;
+            }
+            cout<<"CORRECT\n";
+        }
+    }
 
     return 0;
 }
