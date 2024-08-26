eaadb6131f9c957a1932583bdde4fa156d04be0b
TheLordOfCat <franciszekjarz@gmail.com>
2024-08-26 08:33:26 +0200

    TEL

--- 25th/2stage/tel/tester.cpp
+++ 25th/2stage/tel/tester.cpp
@@ -23,6 +23,18 @@ using namespace std;
 int n, m;
 vector<tuple<char,int,int,int>> promts;
 
+PII& operator+=(PII& lhs, const PII& rhs) {
+    lhs.first += rhs.first;
+    lhs.second += rhs.second;
+    return lhs;
+}
+
+pair<ll, ll>& operator+=(pair<ll, ll>& lhs, const pair<ll, ll>& rhs) {
+    lhs.first += rhs.first;
+    lhs.second += rhs.second;
+    return lhs;
+}
+
 void getData(){
     promts.clear();
 
@@ -197,8 +209,115 @@ vector<int> brute(){
     return ans;
 }
 
-vector<int> solve(){
+vector<ll> tree;
+vector<pair<ll,ll>> lazy;
+ll R = 0;
+int depth = 0;
+ll totalSize = 0;
 
+int left(int v){
+    return 2*v;
+}
+
+int right(int v){
+    return 2*v+1;
+}
+
+int leaf(int v){
+    return R+v;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+void upateTree(int v, int tl, int tr, int l, int r, ll s, ll a){
+    int mid = (tl+tr)/2;
+    if(tl == l && tr == r){
+        lazy[left(v)] += MP(s,a);
+        lazy[right(v)] += MP(s+(a*(mid)), a);
+        ll sum = 0;
+        for(ll i = 0; i<(tr-tl+1); i++){
+            if(s + a*i > 0){
+                sum += s+ a*i;
+            }
+        }
+        tree[v] += sum;
+    }else{
+        upateTree(left(v), tl, mid, l, min(r, mid), s, a);   
+        upateTree(right(v), mid+1, tr, max(l, mid+1), r, s + (mid-tl)*a, a);   
+        tree[v] = tree[left(v)] + tree[right(v)];
+    }
+}
+
+ll query(int v, int tl, int tr, int l, int r){
+    int mid = (tl+tr)/2;
+    if(lazy[v].first != 0 && lazy[v].second != 0){
+        ll s = lazy[v].first;
+        ll a = lazy[v].second;
+
+        ll sum = 0;
+        for(ll i = 0; i<(tr-tl+1); i++){
+            if(s + a*i > 0){
+                sum += s+ a*i;
+            }
+        }
+        tree[v] += sum;
+
+        lazy[left(v)] += MP(s,a);
+        lazy[right(v)] += MP(s+(a*(mid)), a); 
+    }
+    lazy[v].first = 0; lazy[v].second = 0;
+
+    if(tl == l && tr == r){
+        return tree[v];
+    }else{
+        return query(left(v), tl, mid, l,min(r, mid)) + query(left(v), mid+1, tr, max(l, mid+1), r);
+    }
+}
+
+vector<ll> solve(){
+    tree.clear();
+    lazy.clear();
+    R = 0;
+    depth = 0;
+    totalSize = 0;
+
+    //building the tree
+    while((1<<depth) < n){
+        R += (1<<depth);
+        depth++;
+    } 
+    totalSize = R + (1<<depth);
+    tree.assign(totalSize+1, 0);
+    lazy.assign(totalSize+1, MP(0,0));
+
+    //getting the ans
+    vector<pair<ll,ll>> poles(n+1, MP(0,0));
+    vector<ll> ans;
+    for(int i = 0; i<m; i++){
+        char t = get<0>(promts[i]);
+        int a = get<1>(promts[i]), b = get<2>(promts[i]), c = get<3>(promts[i]); 
+        if(t == 'P'){
+            ll len = b/c + 1;
+            upateTree(1,R+1, totalSize, a-len, a, b-len*c, c);
+            upateTree(1,R+1, totalSize, a, a+len, b, c*(-1));
+            poles[a] = MP(b,c);
+        }
+        if(t == 'U'){
+            ll b = poles[a].first, c = poles[a].second; 
+            ll len = b/c + 1;
+            upateTree(1,R+1, totalSize, a-len, a, b-len*c, c);
+            upateTree(1,R+1, totalSize, a, a+len, b, c*(-1));
+
+            poles[a] = MP(0,0);
+        }
+        if(t == 'Z'){
+            ll temp = query(1,R+1, totalSize, a, b);
+        }
+    }
+
+    return ans;
 }
 
 int main()
