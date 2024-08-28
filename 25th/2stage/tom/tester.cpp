7d5abaaf6caef651250ec23aefbeef83cd79dee6
TheLordOfCat <franciszekjarz@gmail.com>
2024-08-28 09:40:58 +0200

    TOM coded

--- 25th/2stage/tom/tester.cpp
+++ 25th/2stage/tom/tester.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 #include <ctime>
 #include <cstdlib>
@@ -87,9 +88,57 @@ PIV brute(){
     return ans;
 }
 
-PIV solve(){
+struct comparePII{
+    bool operator()(const PII a, const PII b){
+        if(a.second == b.second){
+            return a.first > b.first;
+        }
+        return a.second > b.second;
+    }
+};
 
-}
+PIV solve(){
+    vector<vector<int>> index(s+1, vector<int>());
+    vector<PII> poems;
+    for(int i = 0; i<s; i++){
+        poems.PB(MP(i,0));
+    }
+    for(int i = 0; i<n; i++){
+        poems[(a[i]+1)%s].second++;
+        index[(a[i]+1)%s].PB(i+1);
+    }
+
+    priority_queue<PII, vector<PII>, comparePII> Q;
+    for(int i = 0; i<s; i++){
+        if(poems[i].second != 0){
+            Q.push(poems[i]);
+        }
+    }
+
+    PIV ans = MP(0,vector<int>());
+
+    int cur = 0;
+    while(!Q.empty()){
+        PII a = Q.top();
+        Q.pop();
+        if(cur + a.second == s-1){
+            PII b = Q.top();
+            Q.pop();
+
+            ans.second.PB(index[b.first].back());
+            index[b.first].pop_back();
+            b.second--;
+            if(b.second != 0) Q.push(b);
+        }else{
+            ans.second.PB(index[a.first].back());
+            index[a.first].pop_back();
+            a.second--;
+        }
+        if(a.second != 0) Q.push(a);
+    }
+
+    return ans;
+}   
 
 int main()
 {
