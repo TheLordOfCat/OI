083b3b3c528c2cbbeaa00350d49717b2092b2efc
TheLordOfCat <franciszekjarz@gmail.com>
2024-09-25 06:53:58 +0200

    PRO base

--- 28th/2stage/pro/tester.cpp
+++ 28th/2stage/pro/tester.cpp
@@ -1,10 +1,72 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+ull K;
+
+void getData(){
+    cin>>K;
+}
+
+void getRandom(){
+    srand(time(0));
+
+    K = rand()%20+1;
+}
+
+void printData(){
+    cout<<"DATA: \n"<<K<<"\n";
+}
+
+bool verify(vector<vector<int>>& plane){
+
+}
+
+vector<vector<int>> solve(){
+
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int op = 1;
+    for(int test = 1; test <= 1; test++){
+        if(op == 1){
+            getData();
+        }else{
+            getRandom();
+        }
+        vector<vector<int>> ansS = solve();
+        if(verify(ansS)){
+            cout<<"ERROR\n";
+            cout<<"SOLVE: ";
+            cout<<ansS.size()<<"\n";
+            for(int i = 0; i<ansS.size(); i++){
+                for(int j = 0; j<ansS[i].size(); j++){
+                    if(ansS[i][j] == 0){
+                        cout<<"#";
+                    }else{
+                        cout<<".";
+                    }
+                }
+                cout<<"\n";
+            }
+        }
+    }
 
     return 0;
 }
