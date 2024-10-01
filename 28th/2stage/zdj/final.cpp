ff9398adf03b7a2a57dac94468681e720cd14236
TheLordOfCat <franciszekjarz@gmail.com>
2024-10-01 21:22:11 +0200

    ;)

--- 28th/2stage/zdj/final.cpp
+++ 28th/2stage/zdj/final.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    std::cout<<"Hello World";
+    for(int i = 2; i<=100; i+=2){
+        for(int j = 1; j<=100; j+=2){
+            if((i*j*(i+j))%(i*i+j*j) == 0){
+                cout<<i<<" "<<j<<"\n";
+            }
+        }
+    }
 
     return 0;
 }
