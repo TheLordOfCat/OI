f590bde4ca2d2ffbb1c546839e08d314e4c048db
TheLordOfCat <franciszekjarz@gmail.com>
2024-10-11 07:48:20 +0200

    BRUTE coded

--- 29th/2stage/bom/tester.cpp
+++ 29th/2stage/bom/tester.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <tuple> 
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,6 +20,7 @@ const ull ullINF = 18'000'000'000'000'000'000;
 
 int n;
 vector<vector<char>> plane;
+PII P, K;
 
 void getData(){
     cin>>n;
@@ -29,6 +32,12 @@ void getData(){
             char c;
             cin>>c;
             plane[i].PB(c);
+
+            if(c == 'P'){
+                P = MP(i,j);
+            }else if(c == 'K'){
+                K = MP(i,j);
+            }
         }
     }
 }
@@ -53,14 +62,15 @@ void getRandom(){
         }
     }
 
-    PII P = MP(rand()%n+1, rand()%n+1);
-    PII K = P;
+    P = MP(rand()%n+1, rand()%n+1);
+    K = P;
     while(K.first == P.first && K.second == P.second){
         K = MP(rand()%n+1, rand()%n+1);
     }
 
     plane[P.first][P.second] = 'P';
     plane[K.first][K.second] = 'K';
+
 }
 
 void printData(){
@@ -75,7 +85,95 @@ void printData(){
 }
 
 tuple<int, PII, vector<int>> brute(){
+    tuple<int,PII,vector<int>> ans = MT(INF, MP(-1,-1), vector<int>());
 
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            vector<vector<char>> bombPlane = plane;
+            
+            //correct the plane
+            if(bombPlane[i][j] == '.'){
+
+                for(int o = j; o>0; o--){
+                    if(bombPlane[i][o] == '#'){
+                        bombPlane[i][o] = '.';
+                    }else if(bombPlane[i][o] == 'X'){
+                        break;
+                    }
+                }
+                for(int o = j; o<=n; o++){
+                    if(bombPlane[i][o] == '#'){
+                        bombPlane[i][o] = '.';
+                    }else if(bombPlane[i][o] == 'X'){
+                        break;
+                    }
+                }
+                for(int o = i; o>0; o--){
+                    if(bombPlane[o][j] == '#'){
+                        bombPlane[o][j] = '.'; 
+                    }else if(bombPlane[o][j] == 'X'){
+                        break;
+                    }
+                }
+                for(int o = i; o<=n; o++){
+                    if(bombPlane[o][j] == '#'){
+                        bombPlane[o][j] = '.'; 
+                    }else if(bombPlane[o][j] == 'X'){
+                        break;
+                    }
+                }
+            }
+        
+            //find the shortes path
+            vector<vector<PII>> prev(n+1, vector<PII>(n+1, MP(-1,-1)));
+            queue<PII> Q;
+            Q.push(P);
+
+            while(!Q.empty()){
+                PII v = Q.front();
+                Q.pop();
+                if(v.first == K.first && v.second == K.second){
+                    break;
+                }
+
+                for(int o = -1; o<2; o+=2){
+                    if(prev[v.first+o][v.second].first == -1){
+                        Q.push(MP(v.first+o, v.second));
+                        prev[v.first+o][v.second] = v;
+                    }
+                }
+                for(int o = -1; o<2; o+=2){
+                    if(prev[v.first][v.second+o].first == -1){
+                        Q.push(MP(v.first, v.second+o));
+                        prev[v.first][v.second + o] = v;
+                    }
+                }
+            }
+
+            //get the distance 
+            vector<int> dir;
+            PII v = K;
+            while(v.first != P.first && v.second != P.second){
+                PII p = prev[v.first][v.second];
+                if(p.first < v.first){
+                    dir.PB(1);
+                }else if(p.first > v.first){
+                    dir.PB(3);
+                }else if(p.second < v.second){
+                    dir.PB(4);
+                }else{
+                    dir.PB(2);
+                }
+            }
+
+            reverse(dir.begin(), dir.end());
+            if(dir.size() < get<0>(ans)){
+                ans = MT(dir.size(), MP(i,j), dir);
+            }
+        }
+    }
+
+    return ans;
 }
 
 tuple<int, PII, vector<int>> solve(){
