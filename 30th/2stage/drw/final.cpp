5d7269809bc0fb27079d2102f99f93a97cb46cf3
TheLordOfCat <franciszekjarz@gmail.com>
2024-11-20 16:31:13 +0100

    DRW coded

--- 30th/2stage/drw/final.cpp
+++ 30th/2stage/drw/final.cpp
@@ -1,10 +1,96 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bitset>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+int n;
+vector<int> a;
+
+void getData(){
+    a.clear();
+
+    cin>>n;
+    for(int i =0 ; i<n; i++){
+        int temp;
+        cin>>temp;
+        a.PB(temp);
+    }
+}   
+
+int solve(){
+    //get the larges
+    int maxValue = -1;
+    for(int i = 0; i<a.size(); i++){
+        maxValue = max(maxValue, a[i]);
+    }
+
+    //group numbers
+    vector<int> group(2*maxValue+1, 0);
+    for(int i = 0; i<a.size(); i++){
+        group[a[i]]++;
+    }
+    group[maxValue]--;
+
+    //reduce
+    for(int i = 1; i<group.size(); i++){
+        if(group[i] > 2){
+            group[i] -= 2;
+            group[i+i]++;
+        }
+    }
+
+    //process knapsack
+    bitset<2500000> knapsack;
+
+    for(int i = 1; i<group.size(); i++){
+        if(group[i] > 0){
+            bitset<2500000> temp = knapsack;
+            temp <<i;
+            auto next = knapsack|temp;
+            knapsack = next;
+        }
+        if(group[i] == 2){
+            bitset<2500000> temp = knapsack;
+            temp <<i;
+            temp <<i;
+            auto next = knapsack|temp;
+            knapsack = next;
+        }
+    }
+
+    int ans = -1;
+    for(int i = 2500000; i>= 1; i--){
+        if(knapsack.test(i)){
+            ans = i;
+            break;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    getData();
+
+    int ansS = solve();
+    cout<<ansS<<"\n";
 
     return 0;
 }
--- 30th/2stage/drw/tester.cpp
+++ 30th/2stage/drw/tester.cpp
@@ -1,10 +1,118 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bitset>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+
+int n;
+vector<int> a;
+
+void getData(){
+    a.clear();
+
+    cin>>n;
+    for(int i =0 ; i<n; i++){
+        int temp;
+        cin>>temp;
+        a.PB(temp);
+    }
+}   
+
+void getRandom(){
+    a.clear();
+    
+    srand(time(0));
+
+    n = rand()%10+1;
+    for(int i = 0; i<n; i++){
+        int temp = rand()%10+1;
+    }
+}
+
+void printData(){
+    cout<<"DATA: \n";
+    cout<<n<<"\n";
+    for(int i = 0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+int brute(){
+    vector<int> aPrem = a;
+
+    sort(aPrem.begin(), aPrem.end());
+
+    int ans = 0;
+
+    do{
+        int workTime = 0;
+        //simulate
+        int curA = 0, curB = 0;
+        int ind =0;
+        while(ind <n){
+            if(curA > 0 || curB>0){
+                workTime++;
+                curA--;
+                curB--;
+            }
+            if(curA <= 0){
+                curA = aPrem[ind];
+                ind++;
+            }
+            if(curB <= 0 && ind<n){
+                curB = aPrem[ind];
+                ind++;
+            }
+        }
+
+        workTime += max(curA, curB);
+        
+        ans = max(workTime, ans);
+    }while(next_permutation(aPrem.begin(), aPrem.end()));
+
+    return ans;
+}
+
+int solve(){
+
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int op = 1;
+    for(int test = 1; test<=1; test++){
+        cout<<"TEST nr."<<test<<" = ";
+        if(op == 1){
+            getData();
+        }else{
+            getRandom();
+        }
+        int ansB = brute();
+        int ansS = solve();
+        if(ansB != ansS){
+            cout<<"ERROR\n";
+            cout<<"BRUTE: "<<ansB<<"\n";
+            cout<<"SOLVE: "<<ansS<<"\n";
+            printData();
+            return 0;
+        }
+        cout<<"CORRECT\n";
+    }
 
     return 0;
 }
