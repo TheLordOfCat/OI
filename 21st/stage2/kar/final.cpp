95bc49b532df9ccb09086ab147017c31e8f7f169
TheLordOfCat <franciszekjarz@gmail.com>
2024-11-27 06:22:50 +0100

    BRUTE done KAR

--- 21st/stage2/kar/final.cpp
+++ 21st/stage2/kar/final.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
 #define MP make_pair
 #define PII pair<int,int>
 #define PB push_back
@@ -25,138 +28,8 @@ void getData(){
     cin>>m;
 }
 
-vector<vector<vector<int>>> tree;
-vector<pair<vector<int>,vector<int>>> range;
-int R = 1;
-int depth = 1;
-int totalSize;
-
-inline int left(int v){
-    return 2*v;
-}
-
-inline int right(int v){
-    return 2*v+1;
-}
-
-inline int parent(int v){
-    return v/2;
-}
-
-inline int leaf(int v){
-    return v+R+1;
-}
-
-void update(int ind){
-    if(ind <= R){
-        range[ind].first = range[left(ind)].first;
-        range[ind].second = range[right(ind)].second;
-    }
-
-    for(int i = 0; i<2; i++){
-        for(int j = 0; j<2; j++){
-
-            if(ind > R){
-                
-                if(range[ind].second[i] <= range[ind].first[j]){
-                    tree[ind][i][j] = 1;
-                }else{
-                    tree[ind][i][j] = 0;
-                }
-
-            }else{
-
-                int l = left(ind);
-                int r = right(ind);
-                int ok = 0;
-                
-                for(int o = 0; o<2; o++){
-                    for(int t = 0; t<2; t++){
-                        
-                        if(tree[l][i][o] == 1 && tree[r][t][j] == 1){
-                            if(range[l].second[o] <= range[r].first[t]){
-                                ok = 1;
-                            }
-                        }
-
-                    }
-                }
-
-                tree[ind][i][j] = ok;
-
-            }
-        }
-    }
-}
-
-void edit(int v){
-    int V = leaf(v);
-    while(V >= 1){
-        update(V);
-        V = parent(V);
-    }
-}
-
-void replace(int a, int b){
-    swap(range[leaf(a-1)],range[leaf(b-1)]);
-    
-    edit(a-1);
-    edit(b-1);
-}
-
 vector<int> solve(){
-    tree.clear();
-    range.clear();
 
-    //initalizing the tree
-    while(1<<depth < n){
-        R += 1<<depth;
-        depth++;
-    }
-
-    totalSize = R + (1<<depth);
-
-    for(int i =0; i<=totalSize; i++) tree.PB(vector<vector<int>>(2, vector<int>(2,0)));
-    for(int i =0; i<=totalSize; i++){
-        vector<int> t = {INF,INF};
-        range.PB(MP(t,t));
-    }
-    
-    for(int i = 0; i<n; i++){
-        vector<int> t1;
-        t1.PB(cards[i].first); t1.PB(cards[i].second);
-        range[leaf(i)] = MP(t1,t1);
-    }
-
-    for(int i = totalSize; i>=1; i--){
-        update(i);
-    }
-
-    //process requests
-    vector<int> ans;
-    for(int i =0; i<m; i++){
-        int a, b;
-        cin>>a>>b;
-        // change.PB(MP(a,b));
-        // for(int i = 0; i<m; i++){
-            // replace(change[i].first, change[i].second);
-            replace(a, b);
-            int num = 0;
-            for(int i = 0; i<2; i++){
-                for(int j = 0; j<2; j++){
-                    num = max(num, tree[1][i][j]);
-                }
-            }
-            if(num == 1){
-                cout<<"TAK\n";
-            }else{
-                cout<<"NIE\n";
-            }
-            // ans.PB(num);
-        // }
-    }
-
-    return ans;
 }
 
 int main()
--- 21st/stage2/kar/tester.cpp
+++ 21st/stage2/kar/tester.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <algorithm>
 
-#include <ctime>
-#include <cstdlib>
-
 using namespace std;
+
+using ll = long long int;
+using ull = unsigned long long int;
 
 #define MP make_pair
 #define PII pair<int,int>
@@ -16,10 +16,13 @@ const int INF = 2'000'000'000;
 int n;
 vector<PII> cards;
 int m;
-vector<PII> change;
+vector<PII> query;
 
 void getData(){
+    cards.clear(); query.clear();
+
     cin>>n;
+    cards.PB(MP(-1,-1));
     for(int i = 0; i<n; i++){
         int a, b;
         cin>>a>>b;
@@ -29,34 +32,30 @@ void getData(){
     for(int i =0; i<m; i++){
         int a, b;
         cin>>a>>b;
-        change.PB(MP(a,b));
+        query.PB(MP(a,b));
     }
 }
 
 void getRandom(){
-    srand(time(0));
-    cards.clear();
-    change.clear();
+    cards.clear(); query.clear();
 
+    srand(time(0));
+
+    // n = rand()%10+1;
     n = 4;
-    // rand()%10+1;
+    cards.PB(MP(INF,INF));
     for(int i = 0; i<n; i++){
-        int a = rand()%(3*n)+1;
-        int b = rand()%(3*n)+1;
+        int a = rand()%10+1;
+        int b = rand()%10+1;
         cards.PB(MP(a,b));
     }
-    m = rand()%5+1;
+
+    // m = rand()%10+1;
+    m = 4;
     for(int i = 0; i<m; i++){
-        int a = rand()%n+1;
-        int b = rand()%n+1;
-        if(a == b){
-            if(b == n){
-                a--;
-            }else{
-                b++;
-            }
-        }
-        change.PB(MP(a,b));
+        int a = rand()%n + 1;
+        int b = rand()%n + 1;
+        query.PB(MP(a,b));
     }
 }
 
@@ -66,34 +65,33 @@ void printData(){
         cout<<cards[i].first<<" "<<cards[i].second<<"\n";
     }
     cout<<m<<"\n";
-    for(int i = 0; i<change.size(); i++){
-        cout<<change[i].first<<" "<<change[i].second<<"\n";
+    for(int i = 0; i<query.size(); i++){
+        cout<<query[i].first<<" "<<query[i].second<<"\n";
     }
 }
 
-//brute errors
-
-vector<int> brute(){
-    vector<int> ans;
-    vector<PII> car = cards;
+vector<bool> brute(){
+    vector<bool> ans;
+    vector<PII> cB = cards;
 
     for(int i = 0; i<m; i++){
-        //change
-        PII temp = cards[change[i].first-1];
-        car[change[i].first-1] = car[change[i].second-1];
-        car[change[i].second-1] = temp;
+        //swap
+        int s1 = query[i].first, s2 = query[i].second;
+        swap(cB[s1],cB[s2]);
 
-        //verifying
-        int cur = min(car[0].first, car[0].second);
+        //verify
         bool ok = true;
-        for(int j = 1; j<n; j++){
-            if(cur <= min(car[j].first, car[j].second)){
-                cur = min(car[j].first, car[j].second);
-            }else if(cur <= max(car[j].first, car[j].second)){
-                cur = max(car[j].first, car[j].second);
+        int head = -1;
+        for(int j = 1 ;j <=n; j++){
+            int a = cB[j].first, b = cB[j].second;
+            if(a >= head && b >= head){
+                head = min(a,b);
+            }else if(a >= head){
+                head = a;
+            }else if(b >= head){
+                head = b;
             }else{
                 ok = false;
-                break;
             }
         }
         ans.PB(ok);
@@ -102,145 +100,25 @@ vector<int> brute(){
     return ans;
 }
 
-vector<vector<vector<int>>> tree;
-vector<pair<vector<int>,vector<int>>> range;
-int R = 1;
-int depth = 1;
-int totalSize;
-
-inline int left(int v){
-    return 2*v;
-}
-
-inline int right(int v){
-    return 2*v+1;
-}
-
-inline int parent(int v){
-    return v/2;
-}
-
-inline int leaf(int v){
-    return v+R+1;
-}
-
-void update(int ind){
-    if(ind <= R){
-        range[ind].first = range[left(ind)].first;
-        range[ind].second = range[right(ind)].second;
-    }
-
-    for(int i = 0; i<2; i++){
-        for(int j = 0; j<2; j++){
-
-            if(ind > R){
-                
-                if(range[ind].second[i] <= range[ind].first[j]){
-                    tree[ind][i][j] = 1;
-                }else{
-                    tree[ind][i][j] = 0;
-                }
-
-            }else{
-
-                int l = left(ind);
-                int r = right(ind);
-                int ok = 0;
-                
-                for(int o = 0; o<2; o++){
-                    for(int t = 0; t<2; t++){
-                        
-                        if(tree[l][i][o] == 1 && tree[r][t][j] == 1){
-                            if(range[l].second[o] <= range[r].first[t]){
-                                ok = 1;
-                            }
-                        }
-
-                    }
-                }
-
-                tree[ind][i][j] = ok;
-
-            }
-        }
-    }
-}
-
-void edit(int v){
-    int V = leaf(v);
-    while(V >= 1){
-        update(V);
-        V = parent(V);
-    }
-}
-
-void replace(int a, int b){
-    swap(range[leaf(a-1)],range[leaf(b-1)]);
+vector<bool> solve(){
     
-    edit(a-1);
-    edit(b-1);
 }
 
-vector<int> solve(){
-    tree.clear();
-    range.clear();
-    R = 1;
-    depth = 1;
-
-    //initalizing the tree
-    while(1<<depth < n){
-        R += 1<<depth;
-        depth++;
-    }
-
-    totalSize = R + (1<<depth);
-
-    for(int i =0; i<=totalSize; i++) tree.PB(vector<vector<int>>(2, vector<int>(2,0)));
-    for(int i =0; i<=totalSize; i++){
-        vector<int> t = {INF,INF};
-        range.PB(MP(t,t));
-    }
-    
-    for(int i = 0; i<n; i++){
-        vector<int> t1;
-        t1.PB(cards[i].first); t1.PB(cards[i].second);
-        range[leaf(i)] = MP(t1,t1);
-    }
-
-    for(int i = totalSize; i>=1; i--){
-        update(i);
-    }
-
-    //process requests
-    vector<int> ans;
-    for(int i = 0; i<m; i++){
-        replace(change[i].first, change[i].second);
-        int num = 0;
-        for(int i = 0; i<2; i++){
-            for(int j = 0; j<2; j++){
-                num = max(num, tree[1][i][j]);
-            }
-        }
-        ans.PB(num);
-    }
-
-    return ans;
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int op = 0;
-    for(int test = 1; test<=1000; test++){
+    int op = 1;
+    for(int test = 1; test<=1; test++){
         cout<<"TEST nr."<<test<<" = ";
         if(op == 1){
             getData();
         }else{
             getRandom();
         }
-        vector<int> ansB = brute();
-        vector<int> ansS = solve();
+        vector<bool> ansB = brute();
+        vector<bool> ansS = solve();
         for(int i = 0; i <m; i++){
             if(ansB[i] != ansS[i]){
                 cout<<"ERROR\n";
