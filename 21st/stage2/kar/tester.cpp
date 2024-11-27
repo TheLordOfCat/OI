4d5b08c9274b356cdc976b041c53f6158245c1fc
TheLordOfCat <franciszekjarz@gmail.com>
2024-11-27 07:04:17 +0100

    KAR coded

--- 21st/stage2/kar/tester.cpp
+++ 21st/stage2/kar/tester.cpp
@@ -100,8 +100,118 @@ vector<bool> brute(){
     return ans;
 }
 
+vector<vector<vector<bool>>> tree;
+vector<PII> range;
+int R;
+
+int parent(int v){
+    return v/2;
+}
+
+int left(int v){
+    return v*2;
+}
+
+int right(int v){
+    return 2*v+1;
+}
+
+int leaf(int v){
+    return v+R;
+}
+
+vector<vector<bool>> combine(int vL, int vR, vector<vector<int>>& cB){
+    vector<vector<bool>> matrix(2, vector<bool>(2, false));
+    vector<vector<bool>> a = tree[vL], b = tree[vR];
+
+    for(int i = 0; i<2; i++){
+        for(int j = 0; j<2; j++){
+            bool ok = false;
+
+            for(int o = 0; o<2; o++){
+                for(int k = 0; k<2; k++){
+
+                    if(a[i][o] && b[k][j]){
+                        if(cB[range[vL].second][o] <= cB[range[vR].first][k]){
+                            ok = true;
+                            break;
+                        }
+                    }
+
+                }
+
+                if(ok) break;
+            }
+
+            matrix[i][j] = ok;
+        }
+    }
+
+    return matrix;
+}
+
+void buildTree(vector<vector<int>>& cB){
+    R = 0;
+    int depth = 0;
+    while((1<<depth) < n){
+        R += (1<<depth); 
+        depth++;
+    }
+
+    vector<vector<bool>> matrix(2, vector<bool>(2, true));
+    tree.assign(R + (1<<depth), matrix);
+    range.assign(R + (1<<depth), MP(-1,-1));
+
+    for(int i = R + (1<<depth); i>= 1; i++){
+        if(i > R){
+            range[i] = MP(i- R,i - R); 
+        }else{
+            range[i].first = range[left(i)].first;
+            range[i].second = range[right(i)].second;
+            tree[i] = combine(left(i), right(i), cB);
+        }
+    }
+}
+
+void updateTree(int v, vector<vector<int>>& cB){
+    int V = leaf(v);
+    V = parent(V);
+
+    while(V >= 1){
+        vector<vector<bool>> matrix = combine(left(V), right(V), cB);
+        tree[V] = matrix;
+    }   
+}
+
+bool queryTree(){
+    bool ans = false;
+    if(tree[1][0][0] || tree[1][0][1] || tree[1][1][0] || tree[1][1][1]){
+        ans = true;
+    }
+    return ans;
+}
+
 vector<bool> solve(){
-    
+    vector<bool> ans;
+    vector<vector<int>> cB(cards.size(), vector<int>());
+    for(int i =0; i<cards.size(); i++){
+        cB[i].PB(cards[i].first);
+        cB[i].PB(cards[i].second);
+    }
+
+    buildTree(cB);
+    for(int i = 0; i<m; i++){
+        int s1 = query[i].first, s2 = query[i].second;
+        swap(cB[s1], cB[s2]);
+        
+        updateTree(s1, cB);
+        updateTree(s2, cB);
+
+        bool temp = queryTree();
+        ans.PB(temp);
+    }
+
+    return ans;
 }
 
 int main()
