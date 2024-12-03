0e5465862cfccd2d07cbc693a72934be5d1fb6b4
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-03 07:01:34 +0100

    POD coded

--- 22nd/2stage/pod/final.cpp
+++ 22nd/2stage/pod/final.cpp
@@ -17,6 +17,9 @@ int n, k;
 vector<int> r;
 
 void getData(){
+    r.clear();
+
+    r.PB(-1);
     cin>>n>>k;
     for(int i = 0; i<n; i++){
         int a;
@@ -25,8 +28,214 @@ void getData(){
     }
 }
 
-pair<ull,int> solve(){
+vector<ull> tree;
+vector<int> banned;
+int R;
 
+int left(int v){
+    return v*2;
+}
+
+int right(int v){
+    return v*2+1;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+int leaf(int v){
+    return v+R;
+}
+
+PII getRange(int v){ //not optiaml
+    int vL = v;
+    int vR = v;
+
+    while(vL < R){
+        vL = left(vL);
+    }
+
+    while(vR < R){
+        vR = right(vR);
+    }
+
+    return MP(vL,vR);
+}
+
+void buildTree(){
+    tree.clear();
+    banned.clear();
+
+    R = 0;
+    int depth = 0;
+    while((1<<depth) < n){
+        R += (1<<depth);
+        depth++;
+    }
+    
+    int totalSize = R + (1<<depth);
+
+    tree.assign(totalSize, 0);
+    banned.assign(totalSize, 0);    
+}
+
+void updateNode(int v, int val){
+    int V = leaf(v);
+    tree[V] += val;
+    if(tree[V] < 0){
+        banned[V] = 1;
+    }
+
+    while(V >= 1){
+        banned[V] = banned[left(V)] + banned[right(V)];
+        V = parent(V);
+    }
+}
+
+void updateRange(int l, int r, int val){
+    int vL = leaf(l);
+    int vR = leaf(r);
+
+    tree[vL] += val;
+    if(vL != vR){
+        tree[vR] += val;
+    }
+
+    if(tree[vL] < 0){
+        banned[vL] = 1;
+    }
+    if(tree[vR] < 0){
+        banned[vR] = 1;
+    }
+
+    while(parent(vL) != parent(vR)){
+        if(left(parent(vL)) == vL){
+            tree[right(parent(vL))] += val;
+            if(tree[right(parent(vL))]< 0){
+                PII r = getRange(right(parent(vL)));
+                banned[right(parent(vL))] = r.second-r.first+1;
+            }
+        }
+        if(right(parent(vR)) == vR){
+            tree[left(parent(vR))] += val;
+            if(tree[left(parent(vR))]< 0){
+                PII r = getRange(left(parent(vR)));
+                banned[left(parent(vR))] = r.second-r.first+1;
+            }
+        }
+        vL = parent(vL);
+        vR = parent(vR);
+    }
+
+    vL = leaf(l); vR = leaf(vR);
+    while(vL >= 1){
+        banned[vL] = banned[left(vL)] + banned[right(vL)];
+        vL = parent(vL);
+    }
+    while(vR >= 1){
+        banned[vR] = banned[left(vR)] + banned[right(vR)];
+        vR = parent(vR);
+    }
+}
+
+ull queryRanges(){
+    return banned[1];
+}
+
+int queryLongest(int start, int v){
+    //find left and right
+    int V = leaf(v);
+    int leftNode = -1, rightNode = -1;
+    while( V >= 1){
+        if(left(parent(V)) == V && rightNode == -1){
+            PII r = getRange(right(parent(V)));
+            if(banned[right(parent(V))] < r.second-r.first+1){
+                rightNode = right(parent(V));
+            }
+        }
+        if(right(parent(V)) == V && leftNode == -1){
+            PII r = getRange(left(parent(V)));
+            if(banned[left(parent(V))] < r.second-r.first+1){
+                leftNode = left(parent(V));
+            }
+        }
+        V = parent(V);
+    }
+
+    int ans = 0;
+    //process left
+    while(leftNode < R){
+        PII r = getRange(right(leftNode));
+        if(banned[right(leftNode)] <= r.second-r.first-1){
+            leftNode = right(leftNode);
+        }else{
+            leftNode = left(leftNode);
+        }
+    }
+
+    ans= max(start-leftNode+1, ans);
+
+    //process right
+    while(rightNode < R){
+        PII r = getRange(left(rightNode));
+        if(banned[left(rightNode)] <= r.second-r.first-1){
+            rightNode = left(rightNode);
+        }else{
+            rightNode = right(rightNode);
+        }
+    }
+
+    ans= max(rightNode-start+1, ans);
+
+    return ans;
+}
+
+pair<ull,int> solve(){
+    //get range and next
+    vector<PII> range(k+1, MP(-1,-1));
+    vector<int> next(n+1, -1); 
+
+    vector<int> last(k+1, -1);
+    for(int i = r.size(); i>0; i--){
+        int cur = r[i];
+        if(range[cur].second == -1){
+            range[cur].second = i;
+        }
+
+        next[cur] = last[cur];
+        last[cur] = i;
+    }
+
+    for(int i = 1; i<last.size(); i++){
+        range[i].first = last[i];
+    }
+
+    //intnialize the tree
+    buildTree();
+
+    for(int i = 1; i<=k; i++){
+        updateRange(range[i].first+1, range[i].second-1, -1);
+    }
+
+    //process
+    pair<ull,int> ans;
+    ans.first += queryRanges();
+    ans.second += queryLongest(1, n/2);
+
+    for(int i = 2; i<=n; i++){
+        updateNode(i-1, -1);
+        if(next[i-1] == -1){
+            updateRange(i, n, 1);
+        }else{
+            updateRange(i, next[i-1]-1, 1);
+            updateRange(next[i-1], n, -1);
+        }
+        ans.first += queryRanges();
+        ans.second += queryLongest(i, min(n, n/2 + i));
+    }
+
+    return ans;
 }
 
 int main()
