5845b545bfc0c43107b673facf3949b8bc7c8d0a
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-11 08:15:31 +0100

    KON debugigng

--- 25th/2stage/kon/final.cpp
+++ 25th/2stage/kon/final.cpp
@@ -61,7 +61,7 @@ PII solve(){
     reduced.PB(pas[0]);
     PII curMar = MP(-1,-1);
     for(int i =1; i<pas.size(); i++){
-        while(pas[i].second < reduced.back().second){ //reduce group
+        while(pas[i].second <= reduced.back().second){ //reduce group
             reduced.pop_back();
             if(reduced.size() == 0) break;
         }
@@ -80,41 +80,58 @@ PII solve(){
         range.PB({reduced[i].second, -1,i});
     }
     int minOps = marked.size();
-    marked.PB(reduced.size());
-    reduced.PB(MP(INF,INF));
 
     sort(range.begin(), range.end(), compareRanges);
     vector<vector<vector<int>>> groups;
     groups.PB(vector<vector<int>>());
-    vector<PII> g(reduced.size()-1, MP(0,0));
+    vector<PII> g(reduced.size(), MP(0,0));
     int mark = 0;
     for(int i = 0; i< range.size()-1; i++){
-        if(range[i][2] == marked[mark] && range[i][1] == -1){
-            groups.PB(vector<vector<int>>());
-            mark++;
-        }else if(range[i][0] >= reduced[marked[mark]].first){
-            groups.back().PB({range[i][2], range[i+1][2], range[i+1][0]-range[i][0]});
-            if(range[i+1][1] == 1){
-                g[range[i+1][2]].first = groups.back().size();
-            }else{
-                g[range[i+1][2]].second = groups.back().size();
+        if(mark <marked.size()){
+            if(range[i][2] == marked[mark] && range[i][1] == -1){
+                if(mark != marked.size()-1){
+                    groups.PB(vector<vector<int>>());
+                }
+                mark++;
+            }else if(range[i][0] >= reduced[marked[mark]].first){
+                groups.back().PB({range[i][2], range[i+1][2], range[i+1][0]-range[i][0]});
+                if(range[i+1][1] == 1){
+                    g[range[i+1][2]].first = groups.back().size();
+                }else{
+                    g[range[i+1][2]].second = groups.back().size();
+                }
             }
         }
     }
 
     //process groups
     vector<vector<int>> dp(groups.size(), vector<int>());
-    dp.back().PB(0);
-    for(int i = 0; i< groups.back().size(); i++){
-        dp.back().PB(groups.back()[i][2]);
+    for(int i =0 ; i<=groups.back().size(); i++){
+        dp.back().PB(0);
     }
+    for(int i = groups.back().size()-1; i>=0; i--){
+        if(groups.back()[i][1] )
+    }
+    
+
     for(int i = groups.size()-2; i>=0; i--){
         dp[i].PB(0);
         for(int j = 0; j<groups[i].size(); j++){
-            if(reduced[groups[i][j][1]].second < reduced[groups[i+1].front()[0]].first){
-                dp[i].PB(dp[i+1].back()*groups[i][j][2] + dp[i].back());
+            if(i == 0 && j <groups[i].size()-1){
+                if(reduced[groups[i][j][1]].second < reduced[groups[i+1].front()[0]].first){
+                    dp[i].PB(0 + dp[i].back());
+                }else{
+                    int next = dp[i+1][g[groups[i][j][1]].second];
+                    dp[i].PB(next*groups[i][j][2] + dp[i].back());
+                }
             }else{
-                dp[i].PB(dp[i+1][g[groups[i][j][1]].second]*groups[i][j][2] + dp[i].back());
+                if(reduced[groups[i][j][1]].second <= reduced[groups[i+1].front()[0]].first){
+                    int next = dp[i+1].back();
+                    dp[i].PB(next*groups[i][j][2] + dp[i].back());
+                }else{
+                    int next = dp[i+1][g[groups[i][j][1]].second];
+                    dp[i].PB(next*groups[i][j][2] + dp[i].back());
+                }
             }
         }
     }
