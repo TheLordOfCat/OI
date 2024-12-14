f854ccb5c212c2b31e497fd07b9cbac435243a98
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-14 11:26:23 +0100

    GWI coded

--- 26th/2stage/gwi/final.cpp
+++ 26th/2stage/gwi/final.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-#include <cstdlib>
-#include <ctime>
+#include <queue>
 
 using namespace std;
 
@@ -33,8 +31,98 @@ void getData(){
     }
 }
 
-PULLV solve(){
+void correctOps(vector<int>& ops, PII con){
+    if(con.first == 0){
+        PII minOps = MP(-1, INF);
+        for(int i = 0; i< con.second; i++){
+            if(minOps.second > opsCost[i].second){
+                minOps = MP(i,opsCost[i].second);
+            }
+        }        
+        ops[minOps.first] = 1;
+    }else{
+        PII minOps = MP(-1, INF);
+        for(int i = 0; i< con.second; i++){
+            if(minOps.second > opsCost[i].second){
+                minOps = MP(i,opsCost[i].second);
+            }
+        }        
+        ops[minOps.first] = 0;
+    }
+}
 
+PULLV solve(){
+    vector<int> ops;
+    for(int i = 0; i<opsCost.size(); i++){
+        if(opsCost[i].first < opsCost[i].second){
+            ops.PB(0);
+        }else{
+            ops.PB(1);
+        }
+    }
+
+    //first consecutive
+    PII firstCon = MP(ops.front(), 1);
+    for(int i = 1; i<ops.size(); i++){
+        if(ops[i] == firstCon.first){
+            firstCon.second++;
+        }else{
+            break;
+        }
+    }
+
+    //correct if needed
+    if(firstCon.first == 0){
+        if(firstCon.second < s){
+            correctOps(ops, firstCon);
+        }
+    }else{
+        if(firstCon.second < n-s){
+            correctOps(ops, firstCon);            
+        }
+    }
+
+    //finish
+    PULLV ans = MP(0, vector<int>());
+    PII con = MP(ops.front(), 1);
+    deque<int> Q;
+    for(int i = 1; i<=n; i++) Q.PB(i);
+
+    for(int i = 1; i<ops.size(); i++){
+        if(ops[i] == con.first){
+            con.second++;
+        }else{
+            vector<int> moves;
+            if(con.first == 0){
+                for(int i = 0; i<con.second; i++){
+                    if(Q.front() == s){
+                        i--;
+                    }else{
+                        moves.PB(Q.front());
+                    }
+                    Q.pop_front();
+                }
+            }else{
+                for(int i = 0; i<con.second; i++){
+                    if(Q.back() == s){
+                        i--;
+                    }else{
+                        moves.PB(Q.back());
+                    }
+                    Q.pop_back();
+                }
+            }
+
+            while(moves.size() > 0){
+                ans.second.PB(moves.back());
+                moves.pop_back();
+            }
+
+            con = MP(ops[i],1);
+        }
+    }
+
+    return ans;
 }
 
 int main()
