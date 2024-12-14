11626b0a5792245c48893dcf79d0bde4f6201216
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-14 10:07:25 +0100

    BRUTE GWI

--- 26th/2stage/gwi/tester.cpp
+++ 26th/2stage/gwi/tester.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-#include <cstdlib>
-#include <ctime>
+#include <queue>
 
 using namespace std;
 
@@ -56,28 +54,125 @@ void printData(){
     }
 }
 
-PULLV bruteRec(int ind){
-    PULLV ans;
+bool getMovesBrute(PIV& ans, bool& firstOps, PII con, deque<int>& Q){
+    if(firstOps){
+        if(con.first == 0){
+            if(s-1 < con.second){
+                return false;
+            }
+        }else{
+            if(n-s < con.second){
+                return false;
+            }
+        }
 
-    PULLV left = bruteRec(ind+1);
-    PULLV right = bruteRec(ind+1);
+        vector<int> moves;
+        if(con.first == 0){
+            for(int i =0; i< con.second; i++){
+                moves.PB(Q.front());
+                Q.pop_front();
+            }
+        }else{
+            for(int i =0; i< con.second; i++){
+                moves.PB(Q.back());
+                Q.pop_back();
+            }
+        }
 
-    if(left <right){
-        ans = left;
-        ans.first += opsCost[ind].first;
-        left.second.PB(0);
+        while(moves.size() > 0){
+            ans.second.PB(moves.back());
+            moves.pop_back();
+        }
+        firstOps = false;
     }else{
-        ans = right;
-        ans.first += opsCost[ind].second;
-        right.second.PB(1);
+        vector<int> moves;
+        if(con.first == 0){
+            for(int i =0; i< con.second; i++){
+                if(Q.front() != s){
+                    moves.PB(Q.front());
+                }else{
+                    i--;
+                }
+                Q.pop_front();
+            }
+        }else{
+            for(int i =0; i< con.second; i++){
+                if(Q.back() != s){
+                    moves.PB(Q.back());
+                }else{
+                    i--;
+                }
+                Q.pop_back();
+            }
+        }
+
+        while(moves.size() > 0){
+            ans.second.PB(moves.back());
+            moves.pop_back();
+        }
+    }
+
+    return true;
+}
+
+PIV checkBrute(vector<int> ops){
+    PIV ans = MP(0, vector<int>());
+    ans.second.PB(s);
+    deque<int> Q;
+    for(int i = 1; i<=n; i++){
+        Q.PB(i);
+    }
+
+    //get the moves
+    bool ok = true;
+    bool firstOps = true;
+    PII con = MP(ops.front(),1);
+    for(int i = 1; i<ops.size(); i++){
+        if(con.first == ops[i]){
+            con.second++;
+        }else{
+            ok = getMovesBrute(ans, firstOps, con, Q);
+            con = MP(ops[i],1);
+        }
+    }
+    ok = getMovesBrute(ans, firstOps, con, Q);
+
+    if(!ok){
+        return MP(INF,vector<int>());
+    }
+
+    //get the value
+    for(int i =0 ; i<ops.size(); i++){
+        if(ops[i] == 0){
+            ans.first += opsCost[i].first;
+        }else{
+            ans.first += opsCost[i].second;
+        }
     }
 
     return ans;
 }
 
-PULLV brute(){
-    PULLV ans = bruteRec();
+PIV brute(){
+    PIV ans = MP(INF, vector<int>());
 
+    int totalComb = 1<<(n-1);
+    for(int i = 0; i<totalComb; i++){
+        vector<int> ops;
+        for(int j = 0;j <n-1;j ++){
+            if(i&(1<<j)){
+                ops.PB(0);
+            }else{
+                ops.PB(1);
+            }
+        }
+        PIV temp = checkBrute(ops);
+        if(temp.first < ans.first){
+            ans = temp;
+        }
+    }
+
+    return ans;
 }
 
 PULLV solve(){
@@ -98,7 +193,7 @@ int main()
             getRandom();
         }
 
-        PULLV ansB = brute();
+        PIV ansB = brute();
         PULLV ansS = solve();
         if(ansB.first != ansS.first){
             cout<<"ERROR\n";
