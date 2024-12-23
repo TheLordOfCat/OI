e464d83ac761948e351f33f9f5ab1939d5bf3a63
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-23 12:09:07 +0100

    CZW

--- 27th/2stage/czw/final.cpp
+++ 27th/2stage/czw/final.cpp
@@ -1,10 +1,63 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <stack>
+#include <tuple>
+#include <string>
+#include <map>
 
 using namespace std;
 
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define PII pair<int,int>
+#define MP make_pair
+#define PB push_back
+#define MT make_tuple
+
+int m;
+vector<int> code;
+
+void getData(){
+    code.clear();
+
+    cin>>m;
+
+    string temp;
+    cin>>temp;
+    for(int i =0 ; i<temp.size(); i++){
+        code.PB(temp[i]-'0');
+    }
+}
+
+void printData(){
+    cout<<"DATA: \n";
+    cout<<m<<"\n";
+    for(int i = 0; i<code.size(); i++){
+        cout<<code[i];
+    }
+    cout<<"\n";
+}
+
+PII solve(){
+    
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    getData();
+
+    PII ansS = solve();
+    cout<<ansS.first<<"\n"<<ansS.second<<"\n";
 
     return 0;
 }
