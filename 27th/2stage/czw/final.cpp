f753aa9bdc1254e589e0e3345341dc73923cb475
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-24 09:29:47 +0100

    CZW coded

--- 27th/2stage/czw/final.cpp
+++ 27th/2stage/czw/final.cpp
@@ -63,22 +63,8 @@ struct yDominant {
     }
 };
 
-PII solve(){
-    int maxDepth = 0;
-
-    //fill the plane
-    vector<vector<int>> graphBlock(code.size(), vector<int>());
-    for(int i = 0; i<code.size(); i++){
-        if(code[i] == 4){
-            graphBlock[i].PB(i+1);
-            graphBlock[i].PB(i+2);
-            graphBlock[i].PB(i+3);
-            graphBlock[i].PB(i+4);
-        }
-    }
-    
+vector<PII> getPosBlock(vector<vector<int>> &graphBlock, int& globalInd){
     vector<PII> posBlockX(code.size(), MP(-1,-1));
-    int globalInd = 0;
     stack<tuple<int,int,bool>> S;
     S.push(MT(0,0, false));
     while(!S.empty()){
@@ -113,31 +99,117 @@ PII solve(){
             S.push(MT(graphBlock[v][3], 3, false));
         }
     }
+}
 
-    //get area size
-    stack<pair<PII,bool>> Q;
-    Q.push(MP(MP(0,0),false));
+vector<int> getDepth(int& maxDepth){
+    vector<int> depth(code.size(), -1);
+    stack<PII> Q;
+    Q.push(MP(0,0));
     while(!Q.empty()){
-        PII v = Q.top().first;
-        bool b = Q.top().second;
+        PII v = Q.top();
         Q.pop();
+        depth[v.first] = v.second;
+        maxDepth = max(maxDepth, v.second);
 
-        if(b){
-            if(code[v.first] == 4){
-
-            }else{
-                
-            }
-        }
-
-        Q.push(MP(v,true));
         if(code[v.first] == 4){
-            Q.push(MP(MP(v.first+1, v.second+1), false));
-            Q.push(MP(MP(v.first+2, v.second+1), false));
-            Q.push(MP(MP(v.first+3, v.second+1), false));
-            Q.push(MP(MP(v.first+4, v.second+1), false));
+            Q.push(MP(v.first+1, v.second+1));
+            Q.push(MP(v.first+2, v.second+1));
+            Q.push(MP(v.first+3, v.second+1));
+            Q.push(MP(v.first+4, v.second+1));
         }
     }
+
+    return depth;
+}
+
+void connnectEdgeX(map<PII,int, xDominant> &s, int g, int t1, int t2, vector<bool>& con, vector<vector<int>>& graph, int ind){
+    auto lower = s.lower_bound(MP(g, t1));
+    auto upper = s.upper_bound(MP(g, t2));
+
+    for(auto itr = lower; itr != upper; itr++){
+        if(code[itr->second] == 1 && !con[itr->second]){
+            graph[ind].PB(itr->second); 
+            con[itr->second] = true;
+        }
+    }
+}
+
+void connnectEdgeY(map<PII,int, yDominant> &s, int g, int t1, int t2, vector<bool>& con, vector<vector<int>>& graph, int ind){
+    auto lower = s.lower_bound(MP(t1, g));
+    auto upper = s.upper_bound(MP(t2, g));
+
+    for(auto itr = lower; itr != upper; itr++){
+        if(code[itr->second] == 1 && !con[itr->second]){
+            graph[ind].PB(itr->second); 
+            con[itr->second] = true;
+        }
+    }
+}
+
+vector<vector<int>> getGraph(vector<PII> &posBlockX, vector<PII> &posBlockY){
+    vector<vector<int>> graph(code.size(), vector<int>());
+    map<PII,int, xDominant> sX;
+    map<PII,int, yDominant> sY;
+
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 1){
+            sX.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
+            sX.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
+            sY.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
+            sY.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
+        }   
+    }
+
+    for(int i = 0; i<code.size(); i++){
+        int x1 = posBlockX[i].first, x2 = posBlockX[i].second;
+        int y1 = posBlockY[i].first, y2 = posBlockY[i].second;
+        vector<bool> con(code.size(), false);
+        if(code[i] == 1){
+            connnectEdgeX(sX,x1,y1,y2,con,graph,i); 
+            connnectEdgeX(sX,x2,y1,y2,con,graph,i); 
+            connnectEdgeY(sY,y1,x1,x2,con,graph,i); 
+            connnectEdgeY(sY,y2,x1,x2,con,graph,i); 
+        }
+    }
+    return graph;
+}
+
+vector<int> getArea(vector<int>& depth, int maxDepth){
+    vector<int> area;
+    vector<int> depthSup(maxDepth+1, 0);
+    depthSup.back() = 1;
+    for(int i = depthSup.size()-2; i>=0; i--){
+        depthSup[i] = depthSup[i-1]*2;
+        depthSup[i] &= MOD;
+    }
+    
+    for(int i = 0; i<depth.size(); i++){
+        area.PB(depthSup[i]);
+    }
+
+    return area;
+}
+
+PII solve(){
+    int maxDepth = 0;
+
+    //fill the plane
+    vector<vector<int>> graphBlock(code.size(), vector<int>());
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 4){
+            graphBlock[i].PB(i+1);
+            graphBlock[i].PB(i+2);
+            graphBlock[i].PB(i+3);
+            graphBlock[i].PB(i+4);
+        }
+    }
+    
+    //get posBlock
+    int globalInd = 0;
+    vector<PII> posBlockX = getPosBlock(graphBlock, globalInd);
+
+    //get max Depth size
+    vector<int> depth = getDepth(maxDepth);
    
     //rotate
     vector<PII> posBlockY(code.size(), MP(-1,-1));
@@ -146,20 +218,41 @@ PII solve(){
     }
     
     //create graph
-    set<pair<PII,int>, xDominant> sX;
-    set<pair<PII,int>, yDominant> sY;
+    vector<vector<int>> graph = getGraph(posBlockX, posBlockY);
 
-    for(int i = 0; i<code.size(); i++){
-        sX.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
-        sX.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
-        sY.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
-        sY.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
-    }
-
-
+    //get area
+    vector<int> area = getArea(depth, maxDepth);
 
     //get ans
+    PII ans = MP(0,0);
+    vector<bool> used(code.size(), false);
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 1 && !used[i]){
+            stack<int> S;
+            S.push(i);
+            used[i] = true;
+            int maxArea = area[i];
 
+            while(!S.empty()){
+                int v = S.top();
+                S.pop();
+
+                for(int i =0; i<graph[v].size(); i++){
+                    int cur = graph[v][i];
+                    if(!used[cur]){
+                        S.push(cur);
+                        maxArea += area[cur];
+                        area[cur] %= MOD;
+                    }
+                }
+            }
+
+            ans.first++;
+            ans.second = max(ans.second, maxArea);
+        }
+    }
+
+    return ans;
 }
 
 int main()
--- 27th/2stage/czw/tester.cpp
+++ 27th/2stage/czw/tester.cpp
@@ -21,6 +21,8 @@ const ull ullINF = 18'000'000'000'000'000'000;
 #define PB push_back
 #define MT make_tuple
 
+const int MOD = 1'000'000'007;
+
 int m;
 vector<int> code;
 
@@ -163,8 +165,212 @@ PII brute(){
     return ans;
 }
 
-PII solve(){
+struct xDominant {
+    bool operator()(const pair<PII,int>& a, const pair<PII,int>& b) const {
+        if (a.first.first != b.first.first)
+            return a.first.first < b.first.first;
+        return a.first.second < b.first.second;
+    }
+};
+
+struct yDominant {
+    bool operator()(const pair<PII,int>& a, const pair<PII,int>& b) const {
+        if (a.first.second != b.first.second)
+            return a.first.second < b.first.second;
+        return a.first.first < b.first.first;
+    }
+};
+
+vector<PII> getPosBlock(vector<vector<int>> &graphBlock, int& globalInd){
+    vector<PII> posBlockX(code.size(), MP(-1,-1));
+    stack<tuple<int,int,bool>> S;
+    S.push(MT(0,0, false));
+    while(!S.empty()){
+        int v = get<0>(S.top()), type = get<1>(S.top());
+        bool b = get<2>(S.top());
+        S.pop();
+
+        if(b){
+            if(code[v] == 4){
+                int maxim = 0, minim = 0;
+                for(int i =0 ; i<graphBlock[v].size(); i++){
+                    int cur = graphBlock[v][i];
+                    maxim = max(maxim, posBlockX[cur].second);
+                    minim = min(minim, posBlockX[cur].first);
+                }
+                posBlockX[v] = MP(minim, maxim);
+            }else{
+                if(type == 0 || type == 2){
+                    posBlockX[v] = MP(globalInd, globalInd+1);
+                    globalInd++;
+                }else if(type == 1 || type == 3){
+                    posBlockX[v] = posBlockX[v-1];
+                }
+            }
+        }
+
+        S.push(MT(v,type, true));
+        if(graphBlock[v].size() == 4){
+            S.push(MT(graphBlock[v][0], 0, false));
+            S.push(MT(graphBlock[v][1], 1, false));
+            S.push(MT(graphBlock[v][2], 2, false));
+            S.push(MT(graphBlock[v][3], 3, false));
+        }
+    }
+}
+
+vector<int> getDepth(int& maxDepth){
+    vector<int> depth(code.size(), -1);
+    stack<PII> Q;
+    Q.push(MP(0,0));
+    while(!Q.empty()){
+        PII v = Q.top();
+        Q.pop();
+        depth[v.first] = v.second;
+        maxDepth = max(maxDepth, v.second);
+
+        if(code[v.first] == 4){
+            Q.push(MP(v.first+1, v.second+1));
+            Q.push(MP(v.first+2, v.second+1));
+            Q.push(MP(v.first+3, v.second+1));
+            Q.push(MP(v.first+4, v.second+1));
+        }
+    }
+
+    return depth;
+}
+
+void connnectEdgeX(map<PII,int, xDominant> &s, int g, int t1, int t2, vector<bool>& con, vector<vector<int>>& graph, int ind){
+    auto lower = s.lower_bound(MP(g, t1));
+    auto upper = s.upper_bound(MP(g, t2));
+
+    for(auto itr = lower; itr != upper; itr++){
+        if(code[itr->second] == 1 && !con[itr->second]){
+            graph[ind].PB(itr->second); 
+            con[itr->second] = true;
+        }
+    }
+}
+
+void connnectEdgeY(map<PII,int, yDominant> &s, int g, int t1, int t2, vector<bool>& con, vector<vector<int>>& graph, int ind){
+    auto lower = s.lower_bound(MP(t1, g));
+    auto upper = s.upper_bound(MP(t2, g));
+
+    for(auto itr = lower; itr != upper; itr++){
+        if(code[itr->second] == 1 && !con[itr->second]){
+            graph[ind].PB(itr->second); 
+            con[itr->second] = true;
+        }
+    }
+}
+
+vector<vector<int>> getGraph(vector<PII> &posBlockX, vector<PII> &posBlockY){
+    vector<vector<int>> graph(code.size(), vector<int>());
+    map<PII,int, xDominant> sX;
+    map<PII,int, yDominant> sY;
+
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 1){
+            sX.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
+            sX.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
+            sY.insert(MP(MP(posBlockX[i].first, posBlockY[i].first), i));
+            sY.insert(MP(MP(posBlockX[i].second, posBlockY[i].second), i));
+        }   
+    }
+
+    for(int i = 0; i<code.size(); i++){
+        int x1 = posBlockX[i].first, x2 = posBlockX[i].second;
+        int y1 = posBlockY[i].first, y2 = posBlockY[i].second;
+        vector<bool> con(code.size(), false);
+        if(code[i] == 1){
+            connnectEdgeX(sX,x1,y1,y2,con,graph,i); 
+            connnectEdgeX(sX,x2,y1,y2,con,graph,i); 
+            connnectEdgeY(sY,y1,x1,x2,con,graph,i); 
+            connnectEdgeY(sY,y2,x1,x2,con,graph,i); 
+        }
+    }
+    return graph;
+}
+
+vector<int> getArea(vector<int>& depth, int maxDepth){
+    vector<int> area;
+    vector<int> depthSup(maxDepth+1, 0);
+    depthSup.back() = 1;
+    for(int i = depthSup.size()-2; i>=0; i--){
+        depthSup[i] = depthSup[i-1]*2;
+        depthSup[i] &= MOD;
+    }
     
+    for(int i = 0; i<depth.size(); i++){
+        area.PB(depthSup[i]);
+    }
+
+    return area;
+}
+
+PII solve(){
+    int maxDepth = 0;
+
+    //fill the plane
+    vector<vector<int>> graphBlock(code.size(), vector<int>());
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 4){
+            graphBlock[i].PB(i+1);
+            graphBlock[i].PB(i+2);
+            graphBlock[i].PB(i+3);
+            graphBlock[i].PB(i+4);
+        }
+    }
+    
+    //get posBlock
+    int globalInd = 0;
+    vector<PII> posBlockX = getPosBlock(graphBlock, globalInd);
+
+    //get max Depth size
+    vector<int> depth = getDepth(maxDepth);
+   
+    //rotate
+    vector<PII> posBlockY(code.size(), MP(-1,-1));
+    for(int i = 0; i<posBlockX.size(); i++){
+        posBlockY[i] = MP(globalInd-posBlockX[i].first, globalInd-posBlockX[i].second);
+    }
+    
+    //create graph
+    vector<vector<int>> graph = getGraph(posBlockX, posBlockY);
+
+    //get area
+    vector<int> area = getArea(depth, maxDepth);
+
+    //get ans
+    PII ans = MP(0,0);
+    vector<bool> used(code.size(), false);
+    for(int i = 0; i<code.size(); i++){
+        if(code[i] == 1 && !used[i]){
+            stack<int> S;
+            S.push(i);
+            used[i] = true;
+            int maxArea = area[i];
+
+            while(!S.empty()){
+                int v = S.top();
+                S.pop();
+
+                for(int i =0; i<graph[v].size(); i++){
+                    int cur = graph[v][i];
+                    if(!used[cur]){
+                        S.push(cur);
+                        maxArea += area[cur];
+                        area[cur] %= MOD;
+                    }
+                }
+            }
+
+            ans.first++;
+            ans.second = max(ans.second, maxArea);
+        }
+    }
+
+    return ans;
 }
 
 int main()
