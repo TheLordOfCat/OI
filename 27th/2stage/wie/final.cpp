0b0fff2029f3953f6b0e59c894995da70054bab6
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-25 12:54:04 +0100

    WIE failed

--- 27th/2stage/wie/final.cpp
+++ 27th/2stage/wie/final.cpp
@@ -34,25 +34,35 @@ void getData() {
     }
     ll a, b, c, d;
     cin >> a >> b >> c >> d;
-    shortCol = MP(a, b);
-    tallCol = MP(c, d);
+    tallCol = MP(a, b);
+    shortCol = MP(c, d);
 }
 
-ll solve() {
-    ll ans = shortCol.first + tallCol.first;
+ll solvePush(vector<PLL> c){
+    ll ans = shortCol.first + tallCol.first + 1;
 
     //catapilar method
     vector<ll> gaps;
-    for(int i =0; i<col.size()-1; i++){
-        ll len = col[i+1].first - (col[i].first+col[i].second);
+    for(int i =0; i<c.size()-1; i++){
+        ll len = c[i+1].first - (c[i].first+c[i].second);
         gaps.PB(len);
     }
 
     int beg = 0, end = 0;
     queue<PLL> Q;
+    queue<bool> E;
+    ll den = 0;
+    ll excessive = 0;
     ll shortCount = shortCol.first, tallCount = tallCol.first;
-    while(end != col.size()-1){
+    while(end != c.size()-1){
         ll len = gaps[end];
+
+        if(len < 0){
+            end++;
+            ll b = end-beg+1 + shortCol.first + tallCol.first;
+            ans = max(ans, b);
+            continue;
+        }
 
         ll div = len/shortCol.second;
         ll r = len - div*shortCol.second;
@@ -67,23 +77,75 @@ ll solve() {
             }
             tallCount -= t;
             div -= t*2;
-            shortCount -= div;
-            PLL g = MP(t, div);
+            PLL g;
+            if(shortCount == 0){
+                tallCount -= 1;
+                g = MP(t+1, 0);
+                E.push(true);
+                excessive++;
+            }else{
+                shortCount -= div;
+                g = MP(t, div);
+                E.push(false);
+            }
             Q.push(g);
+            end++;
         }else{ // remove gap
-            if(Q.size() == 0){
+            if(Q.size() != 0){
                 PLL v = Q.front();
                 Q.pop();
+
+                bool b = E.front();
+                E.pop();
+                
+                if(b){
+                    if(den > 0){
+                        v.first--;
+                        v.second++;
+                        den--;
+                    }
+                    excessive--;
+                }
 
                 beg++;
                 shortCount += v.first;
                 tallCount += v.second;
+
+                while(den != excessive){
+                    shortCount--;
+                    den++;
+                    tallCount++;
+                }
+            }else{
+                beg++;
+                end++;
             }
         }
 
-        ans = max(ans, end-beg + shortCol.first + tallCol.first);
+        ll b = end-beg+1 + shortCol.first + tallCol.first;
+        ans = max(ans, b);
     }
 
+    return ans;
+}
+
+ll solve() {
+    ll ans = shortCol.first + tallCol.first + 1;
+
+    ll temp = solvePush(col);
+    ans = max(ans, temp);
+
+    vector<PLL> colCopy = col;
+    ll last = colCopy.back().first; 
+    for(int i = colCopy.size()-1; i>=0; i--){
+        colCopy[i].first = last - colCopy[i].first;
+    }
+
+    reverse(colCopy.begin(), colCopy.end());
+
+    temp = solvePush(colCopy);
+    ans = max(ans, temp);
+    
     return ans;
 }
 
--- 27th/2stage/wie/tester.cpp
+++ 27th/2stage/wie/tester.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
 
 using namespace std;
 
@@ -33,8 +34,8 @@ void getData() {
     }
     ll a, b, c, d;
     cin >> a >> b >> c >> d;
-    shortCol = MP(a, b);
-    tallCol = MP(c, d);
+    tallCol = MP(a, b);
+    shortCol = MP(c, d);
 }
 
 bool pairCompare(PLL a, PLL b) {
@@ -49,18 +50,23 @@ void getRandom() {
 
     srand(time(0));
 
+    int len = 20;
     n = rand() % 10 + 1;
-
+    vector<bool> used(len, false);
     for (int i = 0; i < n; i++) {
-        ll a = rand() % n + 1, b = rand() % 3 + 2;
+        ll a = rand() % len + 1, b = rand() % 5 + 2;
+        while(used[a]){
+            a = rand() % len + 1;
+        }
+        used[a] = true;
         col.PB(MP(a, b));
     }
 
     sort(col.begin(), col.end(), pairCompare);
 
-    ll a = rand() % n + 1, b = rand() % 5 + 3, c = rand() % n + 1, d = rand() % 5 + 3;
+    ll a = rand() % n + 1, b = rand() % 5 + 3, c = rand() % n + 1, d = rand() % 2 + 1;
     shortCol = MP(a, b);
-    tallCol = MP(c, d);
+    tallCol = MP(c, d*b);
 }
 
 void printData() {
@@ -69,8 +75,7 @@ void printData() {
     for (int i = 0; i < col.size(); i++) {
         cout << col[i].first << " " << col[i].second << "\n";
     }
-    cout << shortCol.first << " " << shortCol.second << "\n";
-    cout << tallCol.first << " " << tallCol.second << "\n";
+    cout << shortCol.first << " " << shortCol.second << " " << tallCol.first << " " << tallCol.second << "\n";
 }
 
 int brutePush(ll v, vector<PLL>& plane){
@@ -119,7 +124,8 @@ int brutePush(ll v, vector<PLL>& plane){
 }
 
 int brute() {
-    int ans = 0;
+    int ans = shortCol.first + tallCol.first;
+    if(col.size() == 1) ans++;
 
     vector<PLL> colCopy = col;
     for(int i =0 ; i<col.size()-1; i++){
@@ -142,8 +148,115 @@ int brute() {
     return ans;
 }
 
+ll solvePush(vector<PLL> c){
+    ll ans = shortCol.first + tallCol.first + 1;
+
+    //catapilar method
+    vector<ll> gaps;
+    for(int i =0; i<c.size()-1; i++){
+        ll len = c[i+1].first - (c[i].first+c[i].second);
+        gaps.PB(len);
+    }
+
+    int beg = 0, end = 0;
+    queue<PLL> Q;
+    queue<bool> E;
+    ll den = 0;
+    ll excessive = 0;
+    ll shortCount = shortCol.first, tallCount = tallCol.first;
+    while(end != c.size()-1){
+        ll len = gaps[end];
+
+        if(len < 0){
+            end++;
+            ll b = end-beg+1 + shortCol.first + tallCol.first;
+            ans = max(ans, b);
+            continue;
+        }
+
+        ll div = len/shortCol.second;
+        ll r = len - div*shortCol.second;
+        if(r != 0){
+            div++;
+        }
+
+        if(div <= shortCount + tallCount){ //add gap
+            ll t = div/2;
+            if(t > tallCount){
+                t = tallCount;
+            }
+            tallCount -= t;
+            div -= t*2;
+            PLL g;
+            if(shortCount == 0){
+                tallCount -= 1;
+                g = MP(t+1, 0);
+                E.push(true);
+                excessive++;
+            }else{
+                shortCount -= div;
+                g = MP(t, div);
+                E.push(false);
+            }
+            Q.push(g);
+            end++;
+        }else{ // remove gap
+            if(Q.size() != 0){
+                PLL v = Q.front();
+                Q.pop();
+
+                bool b = E.front();
+                E.pop();
+                
+                if(b){
+                    if(den > 0){
+                        v.first--;
+                        v.second++;
+                        den--;
+                    }
+                    excessive--;
+                }
+
+                beg++;
+                shortCount += v.first;
+                tallCount += v.second;
+
+                while(den != excessive){
+                    shortCount--;
+                    den++;
+                    tallCount++;
+                }
+            }else{
+                beg++;
+                end++;
+            }
+        }
+
+        ll b = end-beg+1 + shortCol.first + tallCol.first;
+        ans = max(ans, b);
+    }
+
+    return ans;
+}
+
 ll solve() {
+    ll ans = shortCol.first + tallCol.first + 1;
+
+    ll temp = solvePush(col);
+    ans = max(ans, temp);
+
+    vector<PLL> colCopy = col;
+    ll last = colCopy.back().first; 
+    for(int i = colCopy.size()-1; i>=0; i--){
+        colCopy[i].first = last - colCopy[i].first;
+    }
+
+    reverse(colCopy.begin(), colCopy.end());
+
+    temp = solvePush(colCopy);
+    ans = max(ans, temp);
     
+    return ans;
 }
 
 int main() {
@@ -167,6 +280,7 @@ int main() {
             printData();
             return 0;
         }
+        cout<<"CORRECT\n";
     }
 
     return 0;
