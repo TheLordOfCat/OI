578986b07595d2792250efc6ec92a40e094b366b
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-26 15:56:10 +0100

    TRU coded

--- 27th/2stage/tru/final.cpp
+++ 27th/2stage/tru/final.cpp
@@ -48,13 +48,13 @@ ll numDivisors(ll num){
     return ans;
 }
 
-vector<ll> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
+vector<ll> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43};
 
 void genCand(ll pInd, ll limit, ll v, ll div, vector<PLL>& cand){
     cand.PB(MP(v, div));
 
-    for(int i = 1; i<limit; i++){
-        if(primes[pInd] > n/v){
+    for(int i = 1; i<=limit; i++){
+        if(primes[pInd] > MAXN/v){
             return;
         }
         v *= primes[pInd];
@@ -78,8 +78,45 @@ bool customComparePLL(PLL a, PLL b){
     return a.first < b.first;
 }
 
-tuple<ll,ll,ll> getRange(){
-    
+void genCandP(ll pInd, ll limit, ll base, ll v, ll div, vector<PLL>& cand){
+    if(div >= base){
+        cand.PB(MP(v, div));
+    }
+
+    ll p = 0;
+    while(v*primes[pInd] < limit){
+        if(primes[pInd] > MAXN/v){
+            return;
+        }
+        v *= primes[pInd];
+        p++;
+        genCandP(pInd+1, limit, base, v, div*(p+1), cand);
+    }
+}
+
+
+tuple<ll,ll,ll> getRange(ll antiP, ll divP){
+    vector<PLL> pCand;
+    genCandP(0, antiP, divP/2, 1, 1, pCand);
+
+    tuple<ll, ll, ll> ans = MT(0, 0, 0);
+    for(int i = 0; i<pCand.size(); i++){
+        for(int j = i+1; j<pCand.size(); j++){
+            ll div1 = pCand[i].second;
+            ll div2 = pCand[j].second;
+            ll g = numDivisors(gcd(pCand[i].first, pCand[j].first));
+
+            ll temp = div1 + div2 - g;
+            if( temp > get<0>(ans)){
+                ans = MT(temp, max(pCand[i].first, pCand[j].first), min(pCand[i].first, pCand[j].first));
+            }else if(temp == get<0>(ans)){
+                if(max(pCand[i].first, pCand[j].first) < get<1>(ans)){
+                    ans = MT(temp, max(pCand[i].first, pCand[j].first), min(pCand[i].first, pCand[j].first));
+                }
+            }
+        }
+    }
+    return ans;
 }
 
 tuple<ll, ll, ll> solve() {
@@ -101,17 +138,17 @@ tuple<ll, ll, ll> solve() {
     }
 
     //get antiPrimes
-    vector<ll> antiPrimes;
+    vector<PLL> antiPrimes;
     vector<PLL> antiCand;
     ll l = findLog();
-    genCand(0, l, 1, 1, antiCand);
+    genCand(0, MAXN, 1, 1, antiCand);
 
     sort(antiCand.begin(), antiCand.end(), customComparePLL);
 
     PLL best = antiCand.front();
     for(int i = 1; i<antiCand.size(); i++){
         if(antiCand[i].second > best.second){
-            antiPrimes.PB(best.first);
+            antiPrimes.PB(best);
             best = antiCand[i];
         }
     }
@@ -120,7 +157,7 @@ tuple<ll, ll, ll> solve() {
     vector<tuple<ll, ll, ll>> range;
 
     for(int i = 7; i<antiPrimes.size(); i++){
-        tuple<ll,ll,ll> temp = getRange();
+        tuple<ll,ll,ll> temp = getRange(antiPrimes[i].first, antiPrimes[i].second);
         range.PB(temp);
     }
 
