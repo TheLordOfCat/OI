98a6ee4f3e784a25ea67cbd88039366c85a740d7
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-28 11:35:00 +0100

    PLE coded

--- 28th/2stage/ple/final.cpp
+++ 28th/2stage/ple/final.cpp
@@ -16,6 +16,7 @@ const ull ullINF = 18'000'000'000'000'000'000;
 #define PII pair<int, int>
 #define MP make_pair
 #define PB push_back
+#define PLL pair<ll,ll>
 
 int n;
 vector<ull> w;
@@ -32,186 +33,128 @@ void getData() {
 }
 
 ll R;
-ll depth;
-ll totalSize;
-vector<pair<ll,ll>> difTree;
-vector<ll> changeTree;
-vector<bool> used;
+vector<PLL> segTree;
+vector<ll> lazy;
 
-int leaf(int v){
-    return v + R;
-}
-
-int parent(int v){
-    return v/2;
-}
-
-int left(int v){
+ll left(ll v){
     return v*2;
 }
 
-int right(int v){
+ll right(ll v){
     return v*2+1;
 }
 
-void updateNode(int v){
-    if(difTree[left(v)].first >= 0){
-        difTree[v] = difTree[left(v)];
-    }else if(difTree[right(v)].first >= 0){
-        difTree[v] = difTree[right(v)];
+ll parent(ll v){
+    return v/2;
+}
+
+ll leaf(ll v){
+    return R+v+1;
+}
+
+void updateVer(ll v){
+    if(v > R){
+        return;
+    }
+
+    if(segTree[left(v)].first < segTree[right(v)].first){
+        segTree[v] = segTree[left(v)];
     }else{
-        if(!used[difTree[left(v)].second-R]){
-            difTree[v] = difTree[left(v)];
-        }else{
-            difTree[v] = difTree[right(v)];
-        }
-    } 
-}
-
-void lazyUpadte(int v){
-    if(changeTree[v] != 0){
-        difTree[v].first += changeTree[v];
-        if(v <= R){
-            changeTree[left(v)] += changeTree[v];
-            changeTree[right(v)] += changeTree[v];
-        }
-        changeTree[v] = 0;
+        segTree[v] = segTree[right(v)];
     }
 }
 
-void updateSingle(int v, ll val){
-    used[v] = true;
-    int V = leaf(v);
-    difTree[V] = MP(val,-1);
-    V = parent(V);
+void buildTree(){
+    segTree.clear();
+    lazy.clear();
 
-    while(V >= 1){
-        if(v <= R){
-            lazyUpadte(left(V));
-            lazyUpadte(right(V));
-        }
-        updateNode(V);
-        V = parent(V);
-    }
-    lazyUpadte(1);
-}
-
-void updateRange(int l, int r, int val){
-    int vl = leaf(l);
-    int vr = leaf(r);
-    changeTree[vl] += val;
-    lazyUpadte(vl);
-    if(vl != vr){
-        changeTree[vr] += val;
-        lazyUpadte(vr);
-    }
-
-    while(parent(vl) != parent(vr)){
-        if(vl == left(parent(vl))){
-            changeTree[right(parent(vl))] += val;
-            lazyUpadte(right(parent(vl)));
-        }
-        if(vr == right(parent(vr))){
-            changeTree[left(parent(vr))] += val;
-            lazyUpadte(left(parent(vr)));
-        }
-
-        if(vl <= R && vr <= R){
-            updateNode(vl);
-            updateNode(vr);
-        }
-        lazyUpadte(vl);
-        lazyUpadte(vr);
-
-        vl = parent(vl);
-        vr = parent(vr);
-    }
-
-    lazyUpadte(vl);
-    lazyUpadte(vr);
-    if(vl <= R && vr <= R){
-        updateNode(vl);
-        updateNode(vr);
-    }
-
-    int V = parent(vl);
-    while(V >= 1){
-        lazyUpadte(V);
-        updateNode(V);
-        V = parent(V);
-    }
-}
-
-pair<ll,ll> getRange(int l, int r, int lb, int rb, int v){
-    if(l > r) return MP(-llINF, -1);
-
-    lazyUpadte(v);
-    
-    if(l == lb && r == rb){
-        return difTree[v];
-    }else{
-        int mb = (lb+rb)/2;
-        pair<ll,ll> pl = getRange(l, min(r,mb), lb, mb, left(v));
-        pair<ll,ll> pr = getRange(max(l,mb+1), r, mb+1, rb, right(v));
-
-        if(pl.first >= pr.first){
-            return pl;
-        }else{
-            return pr;
-        }
-    }
-}
-
-vector<ll> solve() {
-    changeTree.clear();
-    difTree.clear();
-    used.clear();
-    
-    //prepare the tree
-    R = 1;
-    depth = 1;
-    while(1<<(depth) < n){
-        R += 1<<depth;
+    int depth = 0;
+    while(1<<depth < n){
+        R += (1<<depth);
         depth++;
-    } 
-    totalSize = R + (1<<depth);
+    }
 
-    //geting the s vector
-    vector<ll> s;
+    segTree.assign(R+(1<<depth)+1, MP(0,0));
+    lazy.assign(R+(1<<depth)+1, 0);
+
+    for(int i = 0; i<w.size(); i++){
+        ll v = leaf(i);
+        segTree[v] = MP(w[i],i);
+    }
+
+    for(int i = R; i>=1; i--){
+        updateVer(i);
+    }
+}
+
+void updateLazy(ll v){
+    if(lazy[v] != 0){
+        segTree[v].first += lazy[v];
+        if(v < R){
+            segTree[left(v)].first += lazy[v];
+            segTree[right(v)].first += lazy[v];
+        }
+        lazy[v] = 0;
+    }
+}
+
+void recAdd(ll v, ll l, ll r, ll L, ll R, ll val){
+    if(r < L || l > R || r < l){
+        return;
+    }
+
+    updateLazy(v);
+
+    if(L <= l && r <= R){
+        segTree[v].first += val;
+        if(v < R){
+            lazy[left(v)] += val;
+            lazy[right(v)] += val;
+        }
+    }else{
+        recAdd(left(v), l, r/2, L, R, val);
+        recAdd(right(v), r/2+1, r, L, R, val);
+        updateVer(v);
+    }
+}
+
+void addRange(ll L, ll R, ll val){
+    recAdd(1, 1, n, L, R, val);
+}
+
+void updateSingle(ll v){
+    ll V = leaf(v);
+
+    if(lazy[V] != 0){
+        segTree[V].first += lazy[V];
+        lazy[V] = 0;
+    }
+    segTree[V].first = -llINF;
+    V = parent(V);
+    while(V >= 1){
+        updateVer(V);
+        updateLazy(V);
+        V = parent(V);
+    }
+}
+
+vector<ll> solveUsed;
+
+vector<ll> solve(){
+    vector<ll> ans;
+    
     ll sum = 0;
-    for(int i = w.size()-1; i>= 0; i--){
-        s.PB(w[i]-sum);
+    for(int i =0 ; i<w.size(); i++){
         sum += w[i];
     }
 
-    reverse(s.begin(),s.end());
-
-    //create seg trees
-    changeTree.assign(totalSize+1, 0);
-    difTree.assign(totalSize+1, MP(-llINF,-1));
-    used.assign(n+1, false);
-    
-    for(int i = totalSize; i>=1; i--){
-        if(i > R && i <= R+n){
-            difTree[i] = MP(s[i-R-1],i);
-        }else if(i <= R){
-            updateNode(i);
-        }
-    }
-
-    //get ans
-    vector<ll> ans;
-    ans.PB(sum);
-    for(int i = 1; i<n; i++){
-        pair<ull,ull> p = getRange(1, n, 1, totalSize - R, 1);
-        ll ind = p.second-R-1;
-        sum -= w[ind];
+    for(int i = 0; i<w.size(); i++){
+        PLL v = segTree[1];
         ans.PB(sum);
-        updateSingle(ind+1, -llINF);
-        updateRange(1, ind, w[ind]);
+        sum -= w[v.second];
+        solveUsed.PB(v.second);
     }
 
-    reverse(ans.begin(), ans.end());
     return ans;
 }
 
