63432b5ef985c9277362405d82be525bb452f879
TheLordOfCat <franciszekjarz@gmail.com>
2024-12-31 11:23:04 +0100

    KON coded

--- 29th/2stage/kon/final.cpp
+++ 29th/2stage/kon/final.cpp
@@ -20,7 +20,7 @@ const ull ullINF = 18'000'000'000'000'000'000;
 int q;
 vector<pair<char,int>> input;
 
-int maxGeusts = 1'000'000;
+int maxGuesses = 1'000'000;
 
 void getData(){
     cin>>q;
@@ -33,120 +33,142 @@ void getData(){
     }
 }
 
-vector<vector<PII>> graphX, graphY;
-vector<PII> pointX, pointY;
-vector<PII> edgesX, edgesY;
-
-void getDominant(vector<vector<PII>>& graph, vector<PII>& point, vector<PII>& edges, vector<bool>& group){
-    int totalGuests = 2;
+vector<int> getGroup(int type){
+    vector<int> ans;
     for(int i = 0; i<input.size(); i++){
-        char c = input[i].first;
-        int x = input[i].second;
-        if(c == 'Z'){
-            totalGuests++;
-            if(group[totalGuests]){
-                graph.PB(vector<PII>());
-                graph.back().PB(MP(edges[x].first, totalGuests));
-                graph[edges[x].first].PB(MP(graph.size()-1, totalGuests));
-
-                graph.back().PB(MP(edges[x].second, x)); 
-                graph[edges[x].second].PB(MP(graph.size()-1, x));
-
-                edges[totalGuests] = MP(edges[x].first, graph.size()-1);
-                edges[x] = MP(graph.size()-1, edges[x].second);
-            }else{
-                point[totalGuests] = point[x];
+        if(input[i].first != '?'){
+            if(type == 0){
+                ans.PB(input[i].second);       
             }
-        }
-        if(c == 'W'){
-            totalGuests++;
-            if(group[totalGuests]){
-                graph.PB(vector<PII>());
-                graph.back().PB(MP(point[x].second, totalGuests));
-                edges[totalGuests] = MP(point[x].second, graph.size()-1);
-                graph[point[x].second].PB(MP(graph.size()-1, totalGuests));
-                point[x] = MP(point[x].first, graph.size()-1);
-            }else{
-                point[totalGuests] = edges[x];
-            }
+            type = (type+1)%2;
         }
     }
+
+    return ans;
 }
 
-pair<vector<int>, vector<PII>> traverseGraph(vector<vector<PII>>& graph, vector<PII>& edges){
+pair<vector<PII>, vector<vector<PII>>> getGraph(vector<int>& mainGroup, int &maxVer){
+    vector<PII> loc(maxVer+1,MP(0,0));
+    vector<PII> point(maxVer+1, MP(0,0));
+    vector<vector<PII>> graph;
+    graph.PB(vector<PII>());
+    graph.PB(vector<PII>());
+    if(mainGroup.front() == 1){
+        graph[0].PB(MP(1,2));
+        point[1] = MP(0,1);
+    }else{
+        graph[0].PB(MP(1,1));
+        point[2] = MP(0,1);
+    }
+
+    vector<bool> part(maxVer+1, false);
+    for(int i = 0; i<mainGroup.size(); i++){
+        part[mainGroup[i]] = true;
+    }
+    
+    int ind = 3;
+    for(int i =0 ; i<input.size(); i++){
+        if(input[i].first == '?'){
+            continue;
+        }
+        int x = input[i].second;
+        if(part[x]){
+            if(input[i].first == 'W'){
+                //extend
+                graph.PB(vector<PII>());
+                graph[point[x].second].PB(MP(graph.size()-1, ind));
+                point[x].second = graph.size()-1;
+            }else{
+                point[ind] = point[x];
+            }
+        }else{
+            if(input[i].first == 'W'){
+                point[ind] = loc[x];
+            }else{
+                //switch
+                graph.PB(vector<PII>());
+                int cur;
+                for(int j = 0; j<graph[loc[x].first].size(); i++){
+                    if(graph[loc[x].first][j].second == x){
+                        cur = j;
+                        break;
+                    }
+                }
+                graph.back().PB(MP(graph[loc[x].first][cur].first, ind));
+                graph[loc[x].first][cur].first = graph.size()-1;
+            }
+        }
+        ind++;
+    }
+    return MP(point, graph);
+}
+
+pair<vector<int>, vector<PII>> getPath(vector<vector<PII>> &graph){
     vector<int> ans1;
-    vector<PII> ans2(graph.size(), MP(-1,-1));
+    vector<PII> ans2;
 
     stack<pair<int,bool>> S;
-    S.push(MP(0, false));
-    vector<bool> vis(graph.size(), false);
+    S.push(MP(0,false));
 
     while(!S.empty()){
         int v = S.top().first;
         bool b = S.top().second;
         S.pop();
-
         if(b){
             ans1.PB((-1)*v);
             ans2[v].second = ans1.size()-1;
             continue;
         }
-        if(vis[v]){
-            continue;
-        }
-        ans1.PB(v);
-        ans2[v].first = ans1.size()-1;
-        vis[v] = true;
 
         S.push(MP(v, true));
         for(int i = 0; i<graph[v].size(); i++){
             PII cur = graph[v][i];
-            if(!vis[cur.first] && ((edges[cur.second].first == v && edges[cur.second].second == cur.first) || (edges[cur.second].second == v && edges[cur.second].first == cur.first))){
-                S.push(MP(cur.first, false));
-            }
+            ans1.PB(cur.second);
+            ans2[v].first = ans1.size()-1;
+            S.push(MP(cur.first, false));
         }
     }
 
     return MP(ans1,ans2);
 }
 
-int parent(int v){
-    return v/2;
-}
-
 int left(int v){
-    return 2*v;
+    return v*2;
 }
 
 int right(int v){
-    return 2*v+1;
+    return v*2+1;
 }
 
 int leaf(int v, int R){
     return R+v;
 }
 
-tuple<vector<int>, int, int> buildTree(vector<int>& trav){
-    vector<int> segTree;
-    int R = 0;
+int parent(int v){
+    return v/2;
+}
+
+vector<int> buildTree(int &R, vector<int>& base){
     int depth = 0;
-    while((1<<depth) < trav.size()){
+    int R = 0;
+    while((1<<depth) < base.size()){
         R += (1<<depth);
         depth++;
     }
-    
-    segTree.assign((R+(1<<depth)), 0);
-    // segTree[leaf(trav.front(), R)] = 1;
-    return MT(segTree, R, depth);
-}
 
-int queryTree(int l, int r, int R, vector<int>& tree){
+    vector<int> tree(R+(1<<depth)+1, 0);
+    return tree;
+}   
+
+int query(int l, int r, int R, vector<int>& tree){
+    int ans = 0;
     int vL = leaf(l, R);
     int vR = leaf(r, R);
 
-    int ans = 0;
     ans += tree[vL];
-    if(vL != vR) ans += tree[vR];
+    if(vL != vR){
+        ans += tree[vR];
+    }
 
     while(parent(vL) != parent(vR)){
         if(left(parent(vL)) == vL){
@@ -158,12 +180,11 @@ int queryTree(int l, int r, int R, vector<int>& tree){
         vL = parent(vL);
         vR = parent(vR);
     }
-
     return ans;
 }
 
-void updateTree(int v, int R, int val, vector<int>& tree){
-    int V = leaf(v, R);
+void updateSingle(int v, int R, vector<int>& tree, int val){
+    int V = leaf(v,R);
 
     while(V >= 1){
         tree[V] += val;
@@ -172,115 +193,60 @@ void updateTree(int v, int R, int val, vector<int>& tree){
 }
 
 vector<int> solve(){
-    graphX.clear(); graphY.clear(); pointX.clear(); pointY.clear(); edgesX.clear(); edgesY.clear();
-
-    graphX.assign(2, vector<PII>()); graphY.assign(2, vector<PII>());
-    pointX.assign(q+1, MP(-1,-1)); pointY.assign(q+1, MP(-1,-1));
-    edgesX.assign(q+1, MP(-1,-1)); edgesY.assign(q+1, MP(-1,-1)); 
-
-    //split guests into groups
-    vector<bool> groupX(q+1, false), groupY(q+1, false); 
-    vector<int> setX = {1};
-    vector<int> setY = {2};
-
-    edgesX[1] = MP(0,1);
-    pointX[2] = MP(0,1);
-    pointY[1] = MP(0,1);
-    edgesY[2] = MP(0,1);
-
-    graphX[0].PB(MP(1,1));
-    graphX[1].PB(MP(0,1));
-    graphY[0].PB(MP(1,2));
-    graphY[1].PB(MP(0,2));
-
-    groupX[1] = true;
-    groupY[2] = true;
-    int totalGuests = 2;
-    for(int i = 0; i<input.size(); i++){
-        char c = input[i].first;
-        int x =  input[i].second;
-        if(c != '?'){
-            totalGuests++;
-            if(groupX[x]){
-                if(c == 'W'){
-                    groupY[totalGuests] = true;
-                    setY.PB(totalGuests);
-                }
-                if(c == 'Z'){
-                    groupX[totalGuests] = true;
-                    setX.PB(totalGuests);
-                }
-            }
-            if(groupY[x]){
-                if(c == 'W'){
-                    groupX[totalGuests] = true;
-                    setX.PB(totalGuests);
-                }
-                if(c == 'Z'){
-                    groupY[totalGuests] = true;
-                    setY.PB(totalGuests);
-                }
-            }
-        }
-    }   
-
-    //create graph
-    getDominant(graphX, pointX, edgesX, groupX);
-    getDominant(graphY, pointY, edgesY, groupY);
-
-    //create seg tree
-    pair<vector<int>, vector<PII>> travX = traverseGraph(graphX, edgesX);
-    pair<vector<int>, vector<PII>> travY = traverseGraph(graphY, edgesY);
-
-    tuple<vector<int>, int, int> segX = buildTree(travX.first);
-    tuple<vector<int>, int, int> segY = buildTree(travY.first);
-
-    //get ans
-    vector<int> ans;
-    totalGuests = 2;
-    //initialize 1
-    updateTree(travX.second[edgesX[1].first].first, get<1>(segX), 1, get<0>(segX));
-    updateTree(travX.second[edgesX[1].first].second, get<1>(segX), -1, get<0>(segX));
-
-    //intailize 2
-    updateTree(travY.second[edgesY[2].first].first, get<1>(segY), 1, get<0>(segY));
-    updateTree(travY.second[edgesY[2].first].second, get<1>(segY), -1, get<0>(segY));
+    //max ver
+    int maxVer = 2;
     for(int i =0; i<input.size(); i++){
-        char c = input[i].first;
-        int x = input[i].second;
-        if(c != '?'){
-            totalGuests++;
-            if(groupX[x]){
-                int R = get<1>(segX);
-                int v1 = travX.second[edgesX[x].first].first;
-                int v2 = travX.second[edgesX[x].first].second;
-                updateTree(v1, R, 1, get<0>(segX));
-                updateTree(v2, R, -1, get<0>(segX));
-            }
-            if(groupY[x]){
-                int R = get<1>(segY);
-                int v1 = travY.second[edgesY[x].first].first;
-                int v2 = travY.second[edgesY[x].first].second;
-                updateTree(v1, R, 1, get<0>(segY));
-                updateTree(v2, R, -1, get<0>(segY));
-            }   
-        }
-        if(c == '?'){
-            int temp;
-            if(groupX[x]){
-                int vt = travY.second[pointY[x].first].first, vb = travY.second[pointY[x].second].second;
-                int R= get<1>(segY);
-                temp = queryTree(vt,vb, R, get<0>(segY));
-            }
-            if(groupY[x]){
-                int vt = travX.second[pointX[x].first].first, vb = travX.second[pointX[x].second].second;
-                int R= get<1>(segX);
-                temp = queryTree(vt, vb, R, get<0>(segX));
-            }
-            ans.PB(temp);
+        if(input[i].first != '?'){
+            maxVer++;
         }
     }
+
+    //split into groups
+    vector<int> X = getGroup(0);
+    vector<int> Y = getGroup(1);
+
+    //get graphs
+    pair<vector<PII>, vector<vector<PII>>> graphX = getGraph(X, maxVer);
+    pair<vector<PII>, vector<vector<PII>>> graphY = getGraph(Y, maxVer);
+
+    //get traversal
+    pair<vector<int>,vector<PII>> pathX = getPath(graphX.second);
+    pair<vector<int>,vector<PII>> pathY = getPath(graphY.second);
+
+    //create segTree
+    int RX, RY;
+    vector<int> treeX = buildTree(RX, pathX.first);
+    vector<int> treeY = buildTree(RY, pathY.first);
     
+    //get ans
+    vector<int> ans;
+    vector<bool> part(maxVer, false);
+    for(int i = 0; i<X.size(); i++){
+        part[X[i]] = true;
+    }
+    int ind = 3;
+    for(int i = 0; i<input.size(); i++){
+        int x = input[i].second;
+        if(input[i].first == '?'){
+            int temp;
+            if(part[x]){
+                temp = query(pathX.second[x].first, pathX.second[x].first, RX, treeX);
+            }else{
+                temp = query(pathY.second[x].first, pathY.second[x].first, RY, treeY);
+            }
+            ans.PB(temp);
+        }else{
+            if(part[ind]){
+                updateSingle(pathY.second[x].first, RY, treeY, 1);
+                updateSingle(pathY.second[x].second, RY, treeY, -1);
+            }else{
+                updateSingle(pathX.second[x].first, RX, treeX, 1);
+                updateSingle(pathX.second[x].second, RX, treeX, -1);
+            }
+            ind++;
+        }
+    }
+
     return ans;
 }
 
--- 29th/2stage/kon/tester.cpp
+++ 29th/2stage/kon/tester.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <tuple>
 
 using namespace std;
 
@@ -90,8 +92,221 @@ vector<int> brute(){
     return ans;
 }
 
-vector<int> solve(){
+vector<int> getGroup(int type){
+    vector<int> ans;
+    for(int i = 0; i<input.size(); i++){
+        if(input[i].first != '?'){
+            if(type == 0){
+                ans.PB(input[i].second);       
+            }
+            type = (type+1)%2;
+        }
+    }
 
+    return ans;
+}
+
+pair<vector<PII>, vector<vector<PII>>> getGraph(vector<int>& mainGroup, int &maxVer){
+    vector<PII> loc(maxVer+1,MP(0,0));
+    vector<PII> point(maxVer+1, MP(0,0));
+    vector<vector<PII>> graph;
+    graph.PB(vector<PII>());
+    graph.PB(vector<PII>());
+    if(mainGroup.front() == 1){
+        graph[0].PB(MP(1,2));
+        point[1] = MP(0,1);
+    }else{
+        graph[0].PB(MP(1,1));
+        point[2] = MP(0,1);
+    }
+
+    vector<bool> part(maxVer+1, false);
+    for(int i = 0; i<mainGroup.size(); i++){
+        part[mainGroup[i]] = true;
+    }
+    
+    int ind = 3;
+    for(int i =0 ; i<input.size(); i++){
+        if(input[i].first == '?'){
+            continue;
+        }
+        int x = input[i].second;
+        if(part[x]){
+            if(input[i].first == 'W'){
+                //extend
+                graph.PB(vector<PII>());
+                graph[point[x].second].PB(MP(graph.size()-1, ind));
+                point[x].second = graph.size()-1;
+            }else{
+                point[ind] = point[x];
+            }
+        }else{
+            if(input[i].first == 'W'){
+                point[ind] = loc[x];
+            }else{
+                //switch
+                graph.PB(vector<PII>());
+                int cur;
+                for(int j = 0; j<graph[loc[x].first].size(); i++){
+                    if(graph[loc[x].first][j].second == x){
+                        cur = j;
+                        break;
+                    }
+                }
+                graph.back().PB(MP(graph[loc[x].first][cur].first, ind));
+                graph[loc[x].first][cur].first = graph.size()-1;
+            }
+        }
+        ind++;
+    }
+    return MP(point, graph);
+}
+
+pair<vector<int>, vector<PII>> getPath(vector<vector<PII>> &graph){
+    vector<int> ans1;
+    vector<PII> ans2;
+
+    stack<pair<int,bool>> S;
+    S.push(MP(0,false));
+
+    while(!S.empty()){
+        int v = S.top().first;
+        bool b = S.top().second;
+        S.pop();
+        if(b){
+            ans1.PB((-1)*v);
+            ans2[v].second = ans1.size()-1;
+            continue;
+        }
+
+        S.push(MP(v, true));
+        for(int i = 0; i<graph[v].size(); i++){
+            PII cur = graph[v][i];
+            ans1.PB(cur.second);
+            ans2[v].first = ans1.size()-1;
+            S.push(MP(cur.first, false));
+        }
+    }
+
+    return MP(ans1,ans2);
+}
+
+int left(int v){
+    return v*2;
+}
+
+int right(int v){
+    return v*2+1;
+}
+
+int leaf(int v, int R){
+    return R+v;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+vector<int> buildTree(int &R, vector<int>& base){
+    int depth = 0;
+    int R = 0;
+    while((1<<depth) < base.size()){
+        R += (1<<depth);
+        depth++;
+    }
+
+    vector<int> tree(R+(1<<depth)+1, 0);
+    return tree;
+}   
+
+int query(int l, int r, int R, vector<int>& tree){
+    int ans = 0;
+    int vL = leaf(l, R);
+    int vR = leaf(r, R);
+
+    ans += tree[vL];
+    if(vL != vR){
+        ans += tree[vR];
+    }
+
+    while(parent(vL) != parent(vR)){
+        if(left(parent(vL)) == vL){
+            ans += tree[right(parent(vL))];
+        }
+        if(right(parent(vR)) == vR){
+            ans += tree[left(parent(vR))];
+        }
+        vL = parent(vL);
+        vR = parent(vR);
+    }
+    return ans;
+}
+
+void updateSingle(int v, int R, vector<int>& tree, int val){
+    int V = leaf(v,R);
+
+    while(V >= 1){
+        tree[V] += val;
+        V = parent(V);
+    }
+}
+
+vector<int> solve(){
+    //max ver
+    int maxVer = 2;
+    for(int i =0; i<input.size(); i++){
+        if(input[i].first != '?'){
+            maxVer++;
+        }
+    }
+
+    //split into groups
+    vector<int> X = getGroup(0);
+    vector<int> Y = getGroup(1);
+
+    //get graphs
+    pair<vector<PII>, vector<vector<PII>>> graphX = getGraph(X, maxVer);
+    pair<vector<PII>, vector<vector<PII>>> graphY = getGraph(Y, maxVer);
+
+    //get traversal
+    pair<vector<int>,vector<PII>> pathX = getPath(graphX.second);
+    pair<vector<int>,vector<PII>> pathY = getPath(graphY.second);
+
+    //create segTree
+    int RX, RY;
+    vector<int> treeX = buildTree(RX, pathX.first);
+    vector<int> treeY = buildTree(RY, pathY.first);
+    
+    //get ans
+    vector<int> ans;
+    vector<bool> part(maxVer, false);
+    for(int i = 0; i<X.size(); i++){
+        part[X[i]] = true;
+    }
+    int ind = 3;
+    for(int i = 0; i<input.size(); i++){
+        int x = input[i].second;
+        if(input[i].first == '?'){
+            int temp;
+            if(part[x]){
+                temp = query(pathX.second[x].first, pathX.second[x].first, RX, treeX);
+            }else{
+                temp = query(pathY.second[x].first, pathY.second[x].first, RY, treeY);
+            }
+            ans.PB(temp);
+        }else{
+            if(part[ind]){
+                updateSingle(pathY.second[x].first, RY, treeY, 1);
+                updateSingle(pathY.second[x].second, RY, treeY, -1);
+            }else{
+                updateSingle(pathX.second[x].first, RX, treeX, 1);
+                updateSingle(pathX.second[x].second, RX, treeX, -1);
+            }
+            ind++;
+        }
+    }
+
+    return ans;
 }
 
 int main()
