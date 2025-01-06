455e20d2fe757d06d76217e9d9523ecee321cec4
TheLordOfCat <franciszekjarz@gmail.com>
2025-01-06 11:06:45 +0100

    KON coded

--- 20th/stage2/kon/tester.cpp
+++ 20th/stage2/kon/tester.cpp
@@ -101,22 +101,143 @@ vector<ll> brute(){
 int N, K;
 vector<int> t;
 
+vector<ll> treeMarked;
+ll RM;
+
+vector<PLL> treeWater;
+ll RW;
+
+int left(int v){
+    return v*2;    
+}
+
+int right(int v){
+    return v*2+1;
+}
+
+int leaf(int v, ll R){
+    return v+ R+1;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+void buildTreeMarked(){
+    int depth = 0;
+    RM = 0;
+    treeMarked.clear();
+    while((1<<depth) < n){
+        RM += (1<<depth);
+        depth++;
+    }
+
+    treeMarked.assign(RM + (1<<depth) + 1, 0);
+}
+
+void updateTW(int v){
+    if(v <= RW){
+        if(treeWater[left(v)].first >= treeWater[right(v)].first){
+            treeWater[v] = treeWater[left(v)];
+        }else{
+            treeWater[v] = treeWater[right(v)];
+        }
+    }
+}
+
+void buildTreeWater(){
+    int depth = 0;
+    RW = 0;
+    treeWater.clear();
+    while((1<<depth) < n){
+        RW += (1<<depth);
+        depth++;
+    }
+
+    treeWater.assign(RW + (1<<depth) + 1, MP(0,0));
+
+    //intialize the tree
+    for(int i = 0; i<t.size(); i++){
+        treeWater[leaf(i,RW)] = MP(t[i], i);
+    }
+
+    for(int i = RW; i>=1; i--){
+        updateTW(i);
+    }
+}
+
 void inicjuj()
 {
     t.assign(n, 0);
     N = n, K = k;
     for (int i = 0; i < n; i++) t[i] = D[i];
+
+    buildTreeMarked();
+    buildTreeWater();
+}
+
+void addTW(int v, int l, int r, int vL, int vR, int val){
+    if(r < vL || vR < l || r<l){
+        return;
+    }
+
+    if(vL<=l && r<=vR){
+        treeWater[v].first += val;
+    }else{
+        int mid = (vL+vR)/2;
+        addTW(left(v), l, r, vL, mid, val);
+        addTW(right(v), l, r, mid+1, r, val);
+    }
+    updateTW(v);
+}
+
+void updateTM(int v){
+    int V = leaf(v, RM);
+    while(V>=1){
+        treeMarked[V] += 1;
+        V = parent(V);
+    }
 }
 
 void podlej(int a, int b)
 {
-    t[a] += 1;
-    t[b] += 1;
+    //range add 1
+    addTW(1, a, b, 0, n, 1);
+
+    //update tree marked
+    while(treeWater[1].first >= K){
+        updateTM(treeWater[1].second);
+        int V = leaf(treeWater[1].second, RW);
+        treeWater[V].first = -INF;
+        while(V >= 1){
+            updateTW(V);
+        }
+    }
 }
 
-int dojrzale(int a, int b)
-{
-    return (t[a] >= K) + (t[b] >= K); /* cos glupiego */
+int dojrzale(int a, int b){
+   int ans = 0;
+   
+   int vA = leaf(a,RM);
+   int vB = leaf(b,RM);
+
+    ans += treeMarked[vA];
+    if(vA != vB){
+        ans += treeMarked[vB];
+    }
+    
+    while(parent(vA) != parent(vB)){
+        if(left(parent(vA)) == vA){
+            ans += right(parent(vA));
+        }
+        if(right(parent(vB)) == vB){
+            ans += left(parent(vA));
+        }
+        vA = parent(vA);
+        vB = parent(vB);
+    }
+
+    return ans;
 }
 
 vector<ll> solve(){
