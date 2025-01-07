4aa6b941fd2fee916d0ae591232d886cf814b940
TheLordOfCat <franciszekjarz@gmail.com>
2025-01-07 07:52:47 +0100

    KON failed

--- 20th/stage2/kon/kon.cpp
+++ 20th/stage2/kon/kon.cpp
@@ -1,19 +1,196 @@
-int N, K, t[10];
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+using ll = long long int;
+using ull = unsigned long long int;
+
+const int INF = 2'000'000'000;
+const ll llINF = 9'000'000'000'000'000'000;
+const ull ullINF = 18'000'000'000'000'000'000;
+
+#define MP make_pair
+#define PII pair<int,int>
+#define PB push_back
+#define MT make_tuple
+#define PLL pair<ll,ll>
+
+int N, K;
+vector<int> t;
+
+vector<ll> treeMarked;
+ll RM;
+
+vector<PLL> treeWater;
+vector<ll> lazy;
+ll RW;
+ll bottomLevel;
+
+int left(int v){
+    return v*2;    
+}
+
+int right(int v){
+    return v*2+1;
+}
+
+int leaf(int v, ll R){
+    return v+ R+1;
+}
+
+int parent(int v){
+    return v/2;
+}
+
+void buildTreeMarked(){
+    int depth = 0;
+    RM = 0;
+    treeMarked.clear();
+    while((1<<depth) < N){
+        RM += (1<<depth);
+        depth++;
+    }
+
+    treeMarked.assign(RM + (1<<depth) + 1, 0);
+}
+
+void updateLazy(int v){
+    if(lazy[v] > 0){
+        treeWater[v].first += lazy[v];
+        if(v <= RW){
+            lazy[left(v)] += lazy[v];
+            lazy[right(v)] += lazy[v];
+        }
+        lazy[v] = 0;
+    }
+}
+
+void updateTW(int v){
+    if(v <= RW){
+        updateLazy(v);
+        updateLazy(left(v));
+        updateLazy(right(v));
+        if(treeWater[left(v)].first >= treeWater[right(v)].first){
+            treeWater[v] = treeWater[left(v)];
+        }else{
+            treeWater[v] = treeWater[right(v)];
+        }
+    }
+}
+
+void buildTreeWater(){
+    int depth = 0;
+    RW = 0;
+    treeWater.clear();
+    while((1<<depth) < N){
+        RW += (1<<depth);
+        depth++;
+    }
+
+    treeWater.assign(RW + (1<<depth) + 1, MP(0,0));
+    lazy.assign(RW + (1<<depth) + 1, 0);
+    bottomLevel = (1<<depth);
+
+    //intialize the tree
+    for(int i = 0; i<t.size(); i++){
+        treeWater[leaf(i,RW)] = MP(t[i], i);
+    }
+
+    for(int i = RW; i>=1; i--){
+        updateTW(i);
+    }
+}
 
 void inicjuj(int n, int k, int *D)
 {
+    t.assign(n, 0);
     N = n, K = k;
-    for (int i = 0; i < n; ++i)
-        t[i] = D[i];
+    for (int i = 0; i < n; i++) t[i] = D[i];
+
+    buildTreeMarked();
+    buildTreeWater();
+}
+
+void addTW(int v, int l, int r, int vL, int vR, int val){
+    if(r < vL || vR < l || r<l){
+        return;
+    }
+
+    if(l<=vL && vR<=r){
+        treeWater[v].first += val;
+        lazy[left(v)] += val;
+        lazy[right(v)] += val;
+    }else{
+        int mid = (vL+vR)/2;
+        addTW(left(v), l, r, vL, mid, val);
+        addTW(right(v), l, r, mid+1, r, val);
+    }
+    updateTW(v);
+}
+
+void updateTM(int v){
+    int V = leaf(v, RM);
+    while(V>=1){
+        treeMarked[V] += 1;
+        V = parent(V);
+    }
+}
+
+void recTW(int v, int e, int l, int r){
+    if(r < e || e < l){
+        return;
+    }
+    
+    updateLazy(v);
+
+    if(l == r && l == e){
+        treeWater[v].first = -INF;
+        updateTM(treeWater[v].second);
+    }else{
+        int mid = (l+r)/2;
+        recTW(left(v), e, l, mid);
+        recTW(right(v), e, mid+1, r);
+    }
+    updateTW(v);
+}
+
+void correctTW(){
+    while(treeWater[1].first >= K){
+        recTW(1, treeWater[1].second, 0, bottomLevel-1);
+    }
 }
 
 void podlej(int a, int b)
 {
-    t[a] += 1;
-    t[b] += 1;
+    //range add 1
+    addTW(1, a, b, 0, bottomLevel-1, 1);
 }
 
-int dojrzale(int a, int b)
-{
-    return (t[a] >= K) + (t[b] >= K); /* cos glupiego */
+int dojrzale(int a, int b){
+    correctTW();
+    int ans = 0;
+    
+    int vA = leaf(a,RM);
+    int vB = leaf(b,RM);
+
+    ans += treeMarked[vA];
+    if(vA != vB){
+        ans += treeMarked[vB];
+    }
+    
+    while(parent(vA) != parent(vB)){
+        if(left(parent(vA)) == vA){
+            ans += treeMarked[right(parent(vA))];
+        }
+        if(right(parent(vB)) == vB){
+            ans += treeMarked[left(parent(vB))];
+        }
+        vA = parent(vA);
+        vB = parent(vB);
+    }
+
+    return ans;
 }
--- 20th/stage2/kon/tester.cpp
+++ 20th/stage2/kon/tester.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -21,22 +24,61 @@ const ull ullINF = 18'000'000'000'000'000'000;
 int n, k;
 vector<int> D;
 int q;
-vector<tuple<int,int,int>> query;
+vector<tuple<char,int,int>> query;
 
 void getData(){
     D.clear(); query.clear();
 
-    cin>>n>>k;
-    for(int i = 0; i<n; i++){
-        int temp;
-        cin>>temp;
-        D.PB(temp);
-    }
-    cin>>q;
-    for(int i =0 ; i<q; i++){
-        int t, a, b;
-        cin>>t>>a>>b;
-        query.PB(MT(t,a,b));
+    if(1){
+        n = 0;
+        ifstream inputFile("kon1ocen.txt");
+        string line;
+
+        getline(inputFile, line);
+        istringstream iss1(line); // Treat the line as a stream
+        int number;
+
+        while (iss1 >> number) { // Extract numbers from the line
+            if(n == 0){
+                n = number;
+            }else{
+                k = number;
+            }
+        }
+
+        getline(inputFile, line);
+        istringstream iss2(line); // Treat the line as a stream
+
+        while (iss2 >> number) { // Extract numbers from the line
+            D.push_back(number);
+        }
+
+        inputFile>>q;
+
+        while (getline(inputFile, line)) { // Read the file line by line
+            istringstream iss3(line); // Treat the line as a stream
+
+            if (line.empty()) continue;
+            char t; int a, b;
+            iss3>>t; iss3>>a; iss3>>b;
+            query.PB(MT(t,a,b));
+        }
+
+        inputFile.close();
+    }else{
+        cin>>n>>k;
+        for(int i = 0; i<n; i++){
+            int temp;
+            cin>>temp;
+            D.PB(temp);
+        }
+        cin>>q;
+        for(int i =0 ; i<q; i++){
+            char t;
+            int a, b;
+            cin>>t>>a>>b;
+            query.PB(MT(t,a,b));
+        }
     }
 }
 
@@ -45,21 +87,29 @@ void getRandom(){
 
     srand(time(0));
 
-    n = 4;
-    k = rand()%10+1;
+    // n = rand()%100+20;
+    n = 2000;
+    // k = rand()%50+15;
+    k = 100;
     for(int i =0 ; i<n; i++){
-        int temp = rand()%10+1;
+        int temp = rand()%100+5;
         D.PB(temp);
     }
 
-    q = rand()%10+1;
+    // q = rand()%10+1;
+    q = 10000;
     for(int i =0 ; i<q; i++){
         int t =rand()%2+1;
         int a = rand()%n, b= rand()%n;
         if(a > b){
             swap(a,b);
         }
-        query.PB(MT(t,a,b));
+        a = b;
+        if(t == 0){
+            query.PB(MT('p',a,b));
+        }else{
+            query.PB(MT('d',a,b));
+        }
     }
 }
 
@@ -80,15 +130,16 @@ vector<ll> brute(){
     vector<ll> ans;
     vector<int> Dcopy = D;
     for(int i = 0; i<q; i++){
-        int t = get<0>(query[i]), a = get<1>(query[i]), b = get<2>(query[i]);
-        if(t == 0){//podlej
+        char t = get<0>(query[i]);
+        int a = get<1>(query[i]), b = get<2>(query[i]);
+        if(t == 'p'){//podlej
             for(int i = a; i<= b; i++){
                 Dcopy[i]++;
             }
         }else{ //dojrzale
             ll count = 0;
-            for(int i = a; i<=b; i++){
-                if(Dcopy[i] >= k){
+            for(int j = a; j<=b; j++){
+                if(Dcopy[j] >= k){
                     count++;
                 }
             }
@@ -105,22 +156,79 @@ vector<ll> treeMarked;
 ll RM;
 
 vector<PLL> treeWater;
+vector<ll> lazy;
 ll RW;
+ll bottomLevel;
 
-int left(int v){
+ll left(ll v){
     return v*2;    
 }
 
-int right(int v){
+ll right(ll v){
     return v*2+1;
 }
 
-int leaf(int v, ll R){
+ll leaf(ll v, ll R){
     return v+ R+1;
 }
 
-int parent(int v){
+ll parent(ll v){
     return v/2;
+}
+
+void updateLazy(ll v){
+    if(lazy[v] > 0){
+        treeWater[v].first += lazy[v];
+        if(v <= RW){
+            lazy[left(v)] += lazy[v];
+            lazy[right(v)] += lazy[v];
+        }
+        lazy[v] = 0;
+    }
+}
+
+void updateTW(ll v){
+    if(v <= RW){
+        updateLazy(v);
+        updateLazy(left(v));
+        updateLazy(right(v));
+        if(treeWater[left(v)].first >= treeWater[right(v)].first){
+            treeWater[v] = treeWater[left(v)];
+        }else{
+            treeWater[v] = treeWater[right(v)];
+        }
+    }
+}
+
+void updateTM(ll v){
+    ll V = leaf(v, RM);
+    while(V>=1){
+        treeMarked[V] += 1;
+        V = parent(V);
+    }
+}
+
+void buildTreeWater(){
+    int depth = 0;
+    RW = 0;
+    treeWater.clear();
+    while((1<<depth) < n){
+        RW += (1<<depth);
+        depth++;
+    }
+
+    treeWater.assign(RW + (1<<depth) + 1, MP(-INF,0));
+    lazy.assign(RW + (1<<depth) + 1, 0);
+    bottomLevel = (1<<depth);
+
+    //intialize the tree
+    for(int i = 0; i<t.size(); i++){
+        treeWater[leaf(i,RW)] = MP(t[i], i);
+    }
+
+    for(int i = RW; i>=1; i--){
+        updateTW(i);
+    }
 }
 
 void buildTreeMarked(){
@@ -135,37 +243,6 @@ void buildTreeMarked(){
     treeMarked.assign(RM + (1<<depth) + 1, 0);
 }
 
-void updateTW(int v){
-    if(v <= RW){
-        if(treeWater[left(v)].first >= treeWater[right(v)].first){
-            treeWater[v] = treeWater[left(v)];
-        }else{
-            treeWater[v] = treeWater[right(v)];
-        }
-    }
-}
-
-void buildTreeWater(){
-    int depth = 0;
-    RW = 0;
-    treeWater.clear();
-    while((1<<depth) < n){
-        RW += (1<<depth);
-        depth++;
-    }
-
-    treeWater.assign(RW + (1<<depth) + 1, MP(0,0));
-
-    //intialize the tree
-    for(int i = 0; i<t.size(); i++){
-        treeWater[leaf(i,RW)] = MP(t[i], i);
-    }
-
-    for(int i = RW; i>=1; i--){
-        updateTW(i);
-    }
-}
-
 void inicjuj()
 {
     t.assign(n, 0);
@@ -176,50 +253,59 @@ void inicjuj()
     buildTreeWater();
 }
 
-void addTW(int v, int l, int r, int vL, int vR, int val){
+void addTW(ll v, ll l, ll r, ll vL, ll vR, ll val){
     if(r < vL || vR < l || r<l){
         return;
     }
 
-    if(vL<=l && r<=vR){
+    if(l<=vL && vR<=r){
         treeWater[v].first += val;
+        lazy[left(v)] += val;
+        lazy[right(v)] += val;
     }else{
-        int mid = (vL+vR)/2;
+        ll mid = (vL+vR)/2;
         addTW(left(v), l, r, vL, mid, val);
         addTW(right(v), l, r, mid+1, r, val);
     }
     updateTW(v);
 }
 
-void updateTM(int v){
-    int V = leaf(v, RM);
-    while(V>=1){
-        treeMarked[V] += 1;
-        V = parent(V);
+void recTW(ll v, ll e, ll l, ll r){
+    if(r < e || e < l){
+        return;
+    }
+    
+    updateLazy(v);
+
+    if(l == r && l == e){
+        treeWater[v].first = -INF;
+        updateTM(treeWater[v].second);
+    }else if(v <= RW){
+        ll mid = (l+r)/2;
+        recTW(left(v), e, l, mid);
+        recTW(right(v), e, mid+1, r);
+    }
+    updateTW(v);
+}
+
+void correctTW(){
+    while(treeWater[1].first >= K){
+        recTW(1, treeWater[1].second, 0, bottomLevel-1);
     }
 }
 
 void podlej(int a, int b)
 {
     //range add 1
-    addTW(1, a, b, 0, n, 1);
-
-    //update tree marked
-    while(treeWater[1].first >= K){
-        updateTM(treeWater[1].second);
-        int V = leaf(treeWater[1].second, RW);
-        treeWater[V].first = -INF;
-        while(V >= 1){
-            updateTW(V);
-        }
-    }
+    addTW(1, a, b, 0, bottomLevel-1, 1);
 }
 
 int dojrzale(int a, int b){
-   int ans = 0;
-   
-   int vA = leaf(a,RM);
-   int vB = leaf(b,RM);
+    correctTW();
+    int ans = 0;
+    
+    ll vA = leaf(a,RM);
+    ll vB = leaf(b,RM);
 
     ans += treeMarked[vA];
     if(vA != vB){
@@ -228,10 +314,10 @@ int dojrzale(int a, int b){
     
     while(parent(vA) != parent(vB)){
         if(left(parent(vA)) == vA){
-            ans += right(parent(vA));
+            ans += treeMarked[right(parent(vA))];
         }
         if(right(parent(vB)) == vB){
-            ans += left(parent(vA));
+            ans += treeMarked[left(parent(vB))];
         }
         vA = parent(vA);
         vB = parent(vB);
@@ -244,8 +330,10 @@ vector<ll> solve(){
     vector<ll> ans;
     inicjuj();
     for(int i = 0; i<q; i++){
-        int t = get<0>(query[i]), a = get<1>(query[i]), b = get<2>(query[i]);
-        if(t == 0){//podlej
+        cout<<i<<"\n";
+        char t = get<0>(query[i]);
+        int a = get<1>(query[i]), b = get<2>(query[i]);
+        if(t == 'p'){//podlej
             podlej(a,b);
         }else{ //dojrzale
             ll count = dojrzale(a,b);
@@ -272,7 +360,7 @@ int main(){
         for(int i = 0; i< ansB.size(); i++){
             if(ansB[i] != ansS[i]){
                 cout<<"ERROR\n";
-                cout<<"BRUTE:";
+                cout<<"BRUTE: ";
                 for(int j = 0; j<ansB.size(); j++){
                     cout<<ansB[j]<<" ";
                 }
