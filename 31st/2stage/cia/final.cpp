eeb2f2101a177a9d0b39f6500c3877d505b700d0
TheLordOfCat <franciszekjarz@gmail.com>
2025-01-16 16:21:09 +0100

    CIA coded

--- 31st/2stage/cia/final.cpp
+++ 31st/2stage/cia/final.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <tuple>
 #include <queue>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -74,7 +76,7 @@ void printData(){
     }
 }
 
-vector<int> baseTree;
+vector<PII> baseTree;
 int R;
 
 int left(int v){
@@ -101,49 +103,173 @@ void buildTree(int interSize){
         depth++;
     }
 
-    baseTree.assign(R+(1<<depth)+1, 0);
+    baseTree.assign(R+(1<<depth)+1, MP(0,0));
 }
 
 void updateVer(int v){
     if(v <= R){
-        baseTree[v] = baseTree[left(v)] + baseTree[right(v)];
+        baseTree[v].first = baseTree[left(v)].first + baseTree[right(v)].first;
+        baseTree[v].second = baseTree[left(v)].second + baseTree[right(v)].second;
     }
 }
 
-vector<vector<int>> verTree;
-
-void queryTree(int val, int treeInd){
-
+void updateSingle(int v, vector<int> indToNum){
+    int V = leaf(v);
+    baseTree[V].first++; baseTree[V].second += indToNum[v];
+    while(V>=1){
+        updateVer(V);
+        V = parent(V);
+    }
 }
 
-vector<int> solve(){
-    //create base tree
-    buildTree();
+vector<vector<PII>> verTree;
 
-    //iterate throught d
-    int type = 1;
-    int curInd = 0;
-    vector<int> dIndexed(n+1, 0);
-    vector<int> oneSeg;
-    for(int i = 0; i<d.size(); i++){
-        if(type == 1){
-            oneSeg.PB(d[i]);
+int findIndex(int val, vector<int>& dIndex){
+    int l = 0, r=  dIndex.size()-1;
+
+    int ans = 0;
+    while(l <=r ){
+        int mid = (l+r)/2;
+        if(dIndex[mid] >= val){
+            ans = mid;
+            l = mid+1;
+        }else{
+            r = mid-1;
         }
-        dIndexed[i] = curInd;
-        curInd += d[i];
-        type = (type+1)%2;
+    }
+    return ans;
+}
+
+PII queryTree(int l, int r, int indT){
+    PII ans = MP(0,0);
+
+    int vL = leaf(l);
+    int vR = leaf(r);
+
+    while(parent(vL) != parent(vR)){
+        if(left(parent(vL)) == vL){
+            ans.first += verTree[indT][right(parent(vL))].first;
+            ans.second += verTree[indT][right(parent(vL))].second;
+        }
+        if(right(parent(vR)) == vR){
+            ans.first += verTree[indT][left(parent(vR))].first;
+            ans.second += verTree[indT][left(parent(vR))].second;
+        }
+        vL = parent(vL);
+        vR = parent(vR);
+    }
+    return ans;
+}
+
+int findD(int t1, int t2, int val){
+    int l = 0, r;
+    if(d.back() == 0){
+        r = (n-1)/2+1;
+    }else{
+        r = (n-2)/2+2;
     }
 
+    int bestInd = 0, ans = 0, left = 0;
+    while(l <= r){
+        int mid = (l+r)/2;
+        PII val1 = queryTree(mid, r, t1), val2 = queryTree(mid, r, t2);
+        if(val2.first - val1.first >=val){
+            bestInd = mid;
+            ans += val2.second - val1.second;
+            left = val - (val2.first - val1.first);
 
+            l = mid-1;
+        }else{
+            l = mid +1;
+        }
+    }
 
-    //process query
-    vector<int> ans;
-    for(int i = 0; i<q; i++){
-        int l = get<0>(query[i]), r = get<1>(query[i]), k = get<2>(query[i]);
-
+    if(bestInd > 0){
+        while(left > 0){
+            ans += d[bestInd-1];
+            left--;
+        }
     }
 
     return ans;
+}
+
+int processQuery(int l, int r, int k, vector<int>& dIndex){
+    //get indexes
+    int indL = findIndex(l, dIndex), indR = findIndex(r, dIndex);
+
+    //get sides
+    int leftSide = 0, rightSide = 0;
+    
+    if(dIndex[indL] != l){
+        int cur = l;
+        while(cur <= dIndex[indL-1] + d[indL-1]){
+            leftSide++;
+            cur++;
+        }
+    }
+    if(dIndex[indR] != r){
+        int cur = l;
+        while(cur >= dIndex[indL-1] + d[indL-1]){
+            leftSide++;
+            cur--;
+        }
+    }
+
+    int temp1 = findD(indL-1, indR, k/2);
+    int temp2 = findD(indL-1, indR, (k-1)/2) + max(leftSide, rightSide);
+    int temp3 = findD(indL-1, indR, (k-2)/2) + leftSide + rightSide;
+
+    return max(temp1,max(temp2,temp3));
+}
+
+vector<int> solve(){
+    //get unice d[i]
+    set<int> S;
+    int type = 1;
+
+    vector<int> dIndexed;
+    int cur = 0;
+    for(int i = 0; i<d.size(); i++){
+        if(type == 1){
+            S.insert(d[i]);
+        }
+        dIndexed.PB(cur);
+        cur += d[i];
+        type = (type+1)%2;
+    }
+    
+    vector<int> indToNum, numToInd(1'000'000'000, -1);
+    for(auto itr = S.begin(); itr != S.end(); itr++){
+        indToNum.PB(*itr);
+        numToInd[*itr] = indToNum.size()-1;
+    }
+
+    //create base tree
+    buildTree(S.size());
+
+    //create prexfix sum tree
+    type = 1;
+    verTree.PB(baseTree);
+    for(int i =0 ; i<n; i++){
+        if(type == 1){
+            updateSingle(numToInd[d[i]], indToNum);
+            verTree.PB(baseTree);
+        }
+        type = (type+1)%2;
+    }
+
+    //process query
+    vector<int> ans;
+    for(int i =0 ;i<q; i++){
+        int l = get<0>(query[i]), r = get<1>(query[i]), k = get<2>(query[i]);
+
+        int temp = processQuery(l, r, k, dIndexed);
+
+        ans.PB(temp);
+    }
+
+    return ans;    
 }
 
 int main()
